694e3ff4f988e149da05424ccbc976fe1feac058
Torsten Irländer <torsten.irlaender@intevation.de>
2013-04-29 14:21:11 +0200

    Initialise the Proben controller.

--- app.js
+++ app.js
@@ -15,4 +15,9 @@ Ext.application({
             ]
         });
     },
+    // Define the controllers of the application. They will be initialized
+    // first before the application "launch" function is called.
+    controllers: [
+        'Proben'
+    ]
 });
