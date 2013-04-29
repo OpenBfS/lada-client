4d1cbea88a49de380e03803f8819f337d150de9e
Torsten Irländer <torsten.irlaender@intevation.de>
2013-04-29 14:20:54 +0200

    Added a simple panel to the application.

--- app.js
+++ app.js
@@ -3,5 +3,16 @@ Ext.application({
     autoCreateViewprt: true,
     launch: function() {
         // Start the application.
-    }
+        console.log('Launching the application');
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    xtype: 'panel',
+                    title: 'Probenauswahlmaske',
+                    html: 'Probenliste wird hier angezeigt.'
+                }
+            ]
+        });
+    },
 });
