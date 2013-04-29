70b899d3e8229ab0d50482f4bde2385f2fb5ee6a
Torsten Irländer <torsten.irlaender@intevation.de>
2013-04-29 15:10:43 +0200

    Map double click event on rows of the userlist to the editProben function.

--- app/controller/Proben.js
+++ app/controller/Proben.js
@@ -8,13 +8,18 @@ Ext.define('Lada.controller.Proben', {
         this.control({
             // CSS like selector to select element in the viewport. See
             // ComponentQuery documentation for more details.
-            'viewport > panel': {
+            'viewport > probenlist': {
                 // Map the "render" event to the given function.
-                render: this.onPanelRendered
+                render: this.onPanelRendered,
+                // Map Doubleclick on rows of the probenlist.
+                itemdblclick: this.editProbe
             }
         });
     },
     onPanelRendered: function() {
         console.log('The panel was rendered');
+    },
+    editProbe: function(grid, record) {
+        console.log('Double click on ' + record.get('name'));
     }
 });
