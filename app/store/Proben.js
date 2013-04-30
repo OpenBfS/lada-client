797469535ad63bd25159938a721b374efe1cbbf1
Torsten Irländer <torsten.irlaender@intevation.de>
2013-04-30 15:06:43 +0200

    Load data from data/proben.json

--- app/store/Proben.js
+++ app/store/Proben.js
@@ -2,6 +2,14 @@ Ext.define('Lada.store.Proben', {
     extend: 'Ext.data.Store',
     fields: ['probeId', 'datenbasisId'],
     model: 'Lada.model.Probe',
-    data  : []
+    autoLoad: true,
+    proxy: {
+        type: 'ajax',
+        url: 'data/proben.json',
+        reader: {
+            type: 'json',
+            root: 'proben'
+        }
+    }
 });
 
