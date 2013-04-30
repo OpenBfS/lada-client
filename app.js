5477a4b160e79a42e2e3fdb384f0172a7b6b8823
Torsten Irländer <torsten.irlaender@intevation.de>
2013-04-30 10:48:44 +0200

    Do not autocreate a viewport as automatic loading of the Viewport.js fiel seems not to work anyway.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 Ext.application({
     name: 'Lada',
-    autoCreateViewprt: true,
+    // Setting this variable to true should trigger loading the Viewport.js
+    // file which sets ob the viewport. But this does not happen.
+    autoCreateViewprt: false,
     launch: function() {
         // Start the application.
         console.log('Launching the application');
