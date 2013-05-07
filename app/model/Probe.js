22b7e865e314e45513088ac129c17c4f78cd7601
Torsten Irländer <torsten.irlaender@intevation.de>
2013-05-07 17:08:08 +0200

    Added more fields to the probenclass

--- app/model/Probe.js
+++ app/model/Probe.js
@@ -1,4 +1,32 @@
 Ext.define('Lada.model.Probe', {
     extend: 'Ext.data.Model',
-    fields: ['datenbasisId', 'probeId']
+    fields: [
+        {name: "baId"},
+        {name: "datenbasisId"},
+        {name: "erzeugerId"},
+        {name: "hauptprobenNr"},
+        {name: "letzteAenderung"},
+        {name: "media"},
+        {name: "mediaDesk"},
+        {name: "mittelungsdauer"},
+        {name: "mpKat"},
+        {name: "mplId"},
+        {name: "mprId"},
+        {name: "mstId"},
+        {name: "netzbetreiberId"},
+        {name: "probeId"},
+        {name: "probeNehmerId"},
+        {name: "probeentnahmeBeginn", type: 'date', convert: ts2date},
+        {name: "probeentnahmeEnde", type: 'date', convert: ts2date},
+        {name: "probenartId"},
+        {name: "solldatumBeginn", type: 'date', convert: ts2date},
+        {name: "solldatumEnde", type: 'date', convert: ts2date},
+        {name: "test"},
+        {name: "umwId"}
+    ]
 });
+
+function ts2date(v, record){
+    // Converts a timestamp into a date object.
+    return new Date(v);
+}
