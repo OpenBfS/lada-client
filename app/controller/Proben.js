9c1ca552257b2eef95c90609ba90ef8f6d49d371
Torsten Irländer <torsten.irlaender@intevation.de>
2013-05-27 14:59:59 +0200

    Load Proben form with detailed version of the proben record.

--- app/controller/Proben.js
+++ app/controller/Proben.js
@@ -38,7 +38,13 @@ Ext.define('Lada.controller.Proben', {
         console.log('Double click on ' + record.get('probeId'));
         // Create new window to edit the seletced record.
         var view = Ext.widget('probenedit');
-        view.down('form').loadRecord(record);
+        //console.log(Ext.ModelManager.getModel('Lada.model.Probe'));
+        Ext.ModelManager.getModel('Lada.model.Probe').load(record.get('probeId'), {
+            success: function(record) {
+                view.down('form').loadRecord(record);
+                console.log("Loaded probe with ID " + record.getId()); //outputs ID
+            }
+        });
     },
     updateProbe: function(button) {
         console.log('Click save');
