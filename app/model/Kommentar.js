2a1bb020d30638d33adf9ba7be90174888cfced7
Torsten Irländer <torsten.irlaender@intevation.de>
2013-05-29 15:19:56 +0200

    Added idProperty to "id" which is the default anyway. Removed api definition and added kId field to the model.

--- app/model/Kommentar.js
+++ app/model/Kommentar.js
@@ -2,17 +2,17 @@ Ext.define('Lada.model.Kommentar', {
     extend: 'Ext.data.Model',
     fields: [
         {name: "id"},
+        {name: "kid"},
         {name: "probeId"},
         {name: "erzeuger"},
         {name: "kdatum"},
         {name: "ktext"}
     ],
+    idProperty: "id",
     proxy: {
         type: 'rest',
         appendId: true, //default
         url: 'server/rest/kommentare',
-        api: {
-        },
         reader: {
             type: 'json'
         }
