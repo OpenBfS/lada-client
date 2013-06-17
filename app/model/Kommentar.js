b58815ac10a13ad5210d7aa7994d74f714ba7160
Torsten Irländer <torsten@irlaender.de>
2013-06-17 13:24:06 +0200

    Changed store. Elements are in thate data attribute. Build custom id attribute.

--- app/model/Kommentar.js
+++ app/model/Kommentar.js
@@ -2,22 +2,25 @@ Ext.define('Lada.model.Kommentar', {
     extend: 'Ext.data.Model',
     fields: [
         {name: "id"},
-        {name: "kid"},
+        {name: "kid", mapping:"id.kid"},
+        {name: "convertedId", convert:buildId},
         {name: "probeId"},
         {name: "erzeuger"},
         {name: "kdatum"},
         {name: "ktext"}
     ],
-    // TODO: This id field is currently a combined field of probeId and kid
-    // which cases problems when generating a request with this id to the
-    // server.
-    idProperty: "id",
+    idProperty: "convertedId",
     proxy: {
         type: 'rest',
         appendId: true, //default
         url: 'server/rest/kommentare',
         reader: {
-            type: 'json'
+            type: 'json',
+            root: 'data'
         }
     }
 });
+
+function buildId(v, record){
+    return record.get('probeId') + record.get('kid');
+}
