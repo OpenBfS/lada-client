041ac1b9dc52f764cde2b6e75df3f787da10436c
Torsten Irländer <torsten.irlaender@intevation.de>
2013-07-04 12:21:51 +0200

    Added missing file.

--- app/view/Viewport.js
+++ app/view/Viewport.js
@@ -9,19 +9,55 @@ Ext.define('Lada.view.Viewport' ,{
 
     initComponent: function() {
         console.log('Setting up Viewport');
-        this.initSearch();
+        //this.initSearch();
 
         // Development related: 
         // Disable "initSearch" call and enable one of the following init
         // methods to get a dialog directly without the need to click through
         // the whole application.
-        //this.initMessung();
+        this.initMessung();
+        //this.initOrt();
 
         this.callParent(arguments);
     },
+    initMesswert: function() {
+        var store = Ext.getStore('Messwerte');
+        store.load({
+            params: {
+                probeId: "000007575853X",
+                messungsId: "1"
+            }
+        });
+    },
+    initOrt: function() {
+        var ort = Ext.create('Lada.model.Ort');
+        var win = Ext.create('Lada.view.orte.Create', {model: ort});
+    },
     initMessung: function() {
-        var messung = Ext.create('Lada.model.Messung');
-        var win = Ext.create('Lada.view.messungen.Create', {model: messung});
+        var store = Ext.getStore('Messungen');
+        var kstore = Ext.getStore('MKommentare');
+        probeId = "000007578314X";
+        store.load({
+            params: {
+                probeId: probeId
+            },
+            callback: function () {
+                console.log(store);
+                var messung = store.data.items[0];
+                console.log(store.data.items[0]);
+                kstore.load({
+                    params: {
+                        probeId: probeId,
+                        messungsId: messung.get('messungsId')
+                    },
+                    callback: function() {
+                        console.log('Creating Messung window');
+                        //var messung = Ext.create('Lada.model.Messung');
+                        var win = Ext.create('Lada.view.messungen.Create', {model: messung});
+                    }
+                });
+            }
+        });
     },
     initSearch: function() {
         this.items = {
---
+++ app/view/mkommentare/List.js
@@ -0,0 +1,6 @@
+Ext.define('Lada.view.mkommentare.List' ,{
+    extend: 'Lada.view.kommentare.List',
+    alias: 'widget.mkommentarelist',
+    store: 'MKommentare',
+    messungsId: null
+});
