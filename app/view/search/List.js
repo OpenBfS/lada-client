8f6cfec0813b7c309ba65cc16492fc7a1cf57bfa
Torsten Irländer <torsten.irlaender@intevation.de>
2013-07-23 09:53:07 +0200

    Use new queries store in search selection.

--- app/view/search/List.js
+++ app/view/search/List.js
@@ -1,23 +1,14 @@
-var searchStore = Ext.create('Ext.data.Store', {
-    fields: ['id', 'name', 'description', 'sql'],
-    data  : [
-        {'id': '1', 'name': 'MST, UWB', 'description': 'Beschreibung der MST, UWB Abfrage', 'sql': 'select * from xxx'},
-        {'id': '2', 'name': 'Rbegin', 'description': 'Beschreibung der Rbegin Abfrage', 'sql': 'select * from xxx'}
-    ]
-});
-
 Ext.define('Lada.view.search.List' ,{
     extend: 'Ext.form.FieldSet',
     title: 'SQL-Auswahl',
     alias: 'widget.queryselector',
-
     initComponent: function() {
         this.items = [
             {
                 id: 'search',
                 xtype: 'combobox',
                 editable: false,
-                store: searchStore,
+                store: 'Queries',
                 displayField:'name',
                 valueField:'id',
                 emptyText:'Wählen Sie eine Abfrage'
