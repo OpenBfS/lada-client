8b58ecdc7ebf186e7dc89edb6867b6b5411926ad
Torsten Irländer <torsten.irlaender@intevation.de>
2013-08-07 12:02:31 +0200

    define lada specific datetimefield and set dateformat to german format.

--- app/view/widgets/Datetime.js
+++ app/view/widgets/Datetime.js
@@ -1,7 +1,8 @@
 // Combobox for Time and Dates
 Ext.define('Lada.view.widgets.Datetime' ,{
-        extend: 'Ext.form.Date',
+        extend: 'Ext.ux.form.DateTimeField',
         alias: 'widget.datetime',
+        format: 'd.m.Y',
         emptyText:'Wählen Sie einen Zeitpunkt',
     initComponent: function() {
         this.callParent(arguments);
