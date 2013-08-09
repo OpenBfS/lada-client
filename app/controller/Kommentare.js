84a037064a08d1005ee086c1b927690f68fd3d28
Torsten Irländer <torsten.irlaender@intevation.de>
2013-08-09 14:58:58 +0200

    Remeoved methods which are defined in the base class.

--- app/controller/Kommentare.js
+++ app/controller/Kommentare.js
@@ -12,6 +12,10 @@ Ext.define('Lada.controller.Kommentare', {
     models: [
         'Kommentar'
     ],
+    init: function() {
+        console.log('Initialising the Kommentare controller');
+        this.callParent();
+    },
     addListeners: function() {
         this.control({
             'kommentarelist': {
@@ -32,11 +36,6 @@ Ext.define('Lada.controller.Kommentare', {
             }
         });
     },
-    saveItem: function(button) {
-        console.log('Saving Kommentar');
-        var form = button.up('window').down('form');
-        form.commit();
-    },
     addItem: function(button) {
         console.log('Adding new Kommentar for Probe ' + button.probeId);
         var kommentar = Ext.create('Lada.model.Kommentar');
@@ -48,38 +47,10 @@ Ext.define('Lada.controller.Kommentare', {
         var view = Ext.widget('kommentarecreate', {model: record});
         console.log("Loaded Kommentar with ID " + record.getId()); //outputs ID
     },
-    deleteItem: function(button) {
-        var grid = button.up('grid');
-        var selection = grid.getView().getSelectionModel().getSelection()[0];
-        Ext.MessageBox.confirm('Löschen', 'Sind Sie sicher?', function(btn){
-            if(btn === 'yes'){
-                var store = grid.getStore();
-                var deleteUrl = selection.getProxy().url + selection.getEidi();
-                Ext.Ajax.request({
-                    url: deleteUrl,
-                    method: 'DELETE',
-                    success: function(response, opts) {
-                        store.reload();
-                    }
-                });
-                console.log('Deleting Kommentar');
-            } else {
-                console.log('Cancel Deleting Kommentar');
-            }
-        });
-    },
     createSuccess: function(form, record, operation) {
         var store = this.getKommentareStore();
         store.reload();
         var win = form.up('window');
         win.close();
-    },
-    createFailure: function(form, record, operation) {
-        Ext.MessageBox.show({
-            title: 'Fehler beim Speichern',
-            msg: form.message,
-            icon: Ext.MessageBox.ERROR,
-            buttons: Ext.Msg.OK
-        });
     }
 });
