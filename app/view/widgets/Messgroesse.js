52badd343c361dd88f9c4e18115b28d9610d5108
Torsten Irländer <torsten.irlaender@intevation.de>
2013-08-13 12:29:56 +0200

    Fixed local filtering of combo

--- app/view/widgets/Messgroesse.js
+++ app/view/widgets/Messgroesse.js
@@ -8,6 +8,13 @@ Ext.define('Lada.view.widgets.Messegroesse' ,{
         displayField: 'messgro0esse',
         valueField: 'messgroesseId',
         emptyText:'Wählen Sie eine Messgröße',
+        forceSelection: true,
+        autoSelect: false,
+        queryMode: 'local',
+        triggerAction : 'all',
+        typeAhead: true,
+        minChars: 0,
+
     initComponent: function() {
         this.callParent(arguments);
     }
