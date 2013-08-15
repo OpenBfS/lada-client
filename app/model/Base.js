a517221a59385782a70b131f054c2c41f0cb2181
Torsten Irländer <torsten.irlaender@intevation.de>
2013-08-15 14:03:47 +0200

    Added helper function to get authorisation information for the current item.

--- app/model/Base.js
+++ app/model/Base.js
@@ -12,5 +12,25 @@ Ext.define('Lada.model.Base', {
      */
     getEidi: function() {
         return "/" + idProperty;
+    },
+    /**
+     * Helper function to make a AJAX request against the authinfo interface
+     * of the server
+     * @param callback function(model, readonly, isowner)
+     * @private
+     */
+    getAuthInfo: function(cb) {
+        Ext.Ajax.request({
+            scope: this,
+            url: 'server/rest/authinfo/' + this.data.probeId,
+            success: function(response, opts) {
+                var obj = Ext.decode(Ext.decode(response.responseText).data);
+                cb(this, obj.readonly, obj.isOwner);
+            },
+            failure: function(response, opts) {
+                console.log('server-side failure with status code ' + response.status);
+                cb(this, true, false);
+            }
+        });
     }
 });
