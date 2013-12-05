2d7a9fd537c14c836bf00b30669dd40c258a155b
Raimund Renkert <raimund.renkert@intevation.de>
2013-12-05 09:56:50 +0100

    New view to create ort details.

---
+++ app/view/orte/CreateOrt.js
@@ -0,0 +1,66 @@
+/**
+ *
+ */
+Ext.define('Lada.view.orte.CreateOrt', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.createortdetail',
+    title: 'Neuer Ort',
+
+    /**
+     * @private
+     * Initialize the view.
+     */
+    initComponent: function() {
+        var me = this;
+
+        var form = Ext.create('Ext.form.Panel', {
+            items: [{
+                xtype: 'textfield',
+                maxLength: 100,
+                name: 'ort_beschreibung',
+                fieldLabel: 'Beschreibung'
+            },
+            {
+                xtype: 'staat',
+                name: 'ort_staatId',
+                fieldLabel: 'Staat'
+            },
+            {
+                xtype: 'verwaltungseinheiten',
+                name: 'ort_gemId',
+                fieldLabel: 'Gemeinde'
+            },
+            {
+                xtype: 'numberfield',
+                name: 'ort_latitude',
+                fieldLabel: 'Lat'
+            },
+            {
+                xtype: 'numberfield',
+                name: 'ort_longitude',
+                fieldLabel: 'Lon'
+            },
+            {
+                xtype: 'numberfield',
+                name: 'ort_hoeheLand',
+                fieldLabel: 'Höhe'
+            }]
+        });
+
+        me.items = [form];
+        this.buttons = [
+            {
+                text: 'Speichern',
+                scope: form,
+                action: 'save'
+            },
+            {
+                text: 'Abbrechen',
+                scope: this,
+                handler: this.close
+            }
+        ];
+        this.callParent(arguments);
+    }
+});
+
