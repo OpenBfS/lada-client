d4093b5a4f2f186fa7117e8163e2bfafc14bb420
Raimund Renkert <raimund.renkert@intevation.de>
2014-12-12 12:18:22 +0100

    Code cleanup.

--- app/view/proben/Edit.js
+++ app/view/proben/Edit.js
@@ -18,8 +18,6 @@ Ext.define('Lada.view.proben.Edit', {
     ],
 
     title: 'Maske für §3-Proben',
-    // Make size of the dialog dependend of the available space.
-    // TODO: Handle resizing the browser window.
     autoShow: true,
     autoScroll: true,
     modal: true,
@@ -34,101 +32,12 @@ Ext.define('Lada.view.proben.Edit', {
             scope: this,
             handler: this.close
         }];
-        this.width = 700// Ext.getBody().getViewSize().width - 30;
+        this.width = 700;
         this.height = Ext.getBody().getViewSize().height - 30;
         // InitialConfig is the config object passed to the constructor on
         // creation of this window. We need to pass it throuh to the form as
         // we need the "modelId" param to load the correct item.
 
-        /*
-        this.items = [{
-            xtype: 'fieldset',
-            title: 'Probenangaben',
-            layout: 'hbox',
-            defaults: {
-                labelWidth: 150
-            },
-            items: [{
-                    layout: 'vbox',
-                    border: 0,
-                    items: [{
-                        xtype: 'mst',
-                        name: 'mstId',
-                        fieldLabel: 'Messstelle',
-                        allowBlank: false
-                    }, {
-                        xtype: 'textfield',
-                        name: 'hauptprobenNr',
-                        maxLength: 20,
-                        fieldLabel: 'Hauptprobennr.'
-                    }]
-            }]
-            */
-/*
-            items: [{
-                layout: 'hbox',
-                border: 0,
-                items: [{
-                    layout: 'vbox',
-                    border: 0,
-                    items: [{
-                        xtype: 'mst',
-                        name: 'mstId',
-                        fieldLabel: 'Messstelle',
-                        allowBlank: false
-                    }, {
-                        xtype: 'textfield',
-                        name: 'hauptprobenNr',
-                        maxLength: 20,
-                        fieldLabel: 'Hauptprobennr.'
-                    }]
-                }, {
-                    xtype: 'fieldset',
-                    title: 'Erweiterte Probenangaben',
-                    collapsible: true,
-                    collapsed: true,
-                    items: [{
-                        xtype: 'datenbasis',
-                        id: 'datenbasis',
-                        editable: false,
-                        name: 'datenbasisId',
-                        fieldLabel: 'Datenbasis'
-                    }, {
-                        xtype: 'betriebsart',
-                        name: 'baId',
-                        fieldLabel: 'Betriebsart'
-                    }, {
-                        xtype: 'testdatensatz',
-                        name: 'test',
-                        fieldLabel: 'Testdatensatz',
-                        allowBlank: false
-                    }, {
-                        xtype: 'probenart',
-                        id: 'probenart',
-                        editable: false,
-                        name: 'probenartId',
-                        fieldLabel: 'Probenart',
-                        allowBlank: false
-                    }, {
-                        xtype: 'numberfield',
-                        allowDecimals: false,
-                        name: 'probeNehmerId',
-                        fieldLabel: 'Probennehmer'
-                    }, {
-                        xtype: 'netzbetreiber',
-                        name: 'netzbetreiberId',
-                        editable: false,
-                        fieldLabel: 'Netzbetreiber',
-                        allowBlank: false
-                    }, {
-                        xtype: 'textfield',
-                        name: 'x11',
-                        fieldLabel: 'Datensatzerzeuger'
-                    }]
-                }]
-            }]
-            */
-//        }];
         var form = Ext.create('Lada.view.proben.EditForm',
             this.initialConfig);
         this.items = [{
