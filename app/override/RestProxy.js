a9236d645fe571baac3a160cea5c7e8f98509794
Andre Heinecke <andre.heinecke@intevation.de>
2015-03-13 17:17:09 +0100

    Improve error handling on authentication loss.

--- app/override/RestProxy.js
+++ app/override/RestProxy.js
@@ -15,20 +15,31 @@ Ext.define('Lada.override.RestProxy', {
     },
 
     processResponse: function (success, operation, request, response, callback, scope) {
-        if (!success && response.status == 401) {
+        /* Check if we were authenticated at one point (Lada.openIDParams) and
+         * if the response means that we lost that authentcation */
+        if (Lada.openIDParams && !success && response.status == 401) {
             var json = Ext.decode(response.responseText);
             if (json) {
-                if (json.message == "699") {
+                if (json.message === "699" || json.message === "698") {
                     /* This is the unauthorized message with the authentication
                      * redirect in the data */
 
                     /* We decided to handle this with a redirect to the identity
                      * provider. In which case we have no other option then to
                      * handle it here with relaunch. */
-                    Lada.launch(); // Data loss!
+                    Ext.MessageBox.confirm('Erneutes Login erforderlich',
+                        'Der Server konnte die Anfrage nicht authentifizieren.<br/>'+
+                        'Für ein erneutes Login muss die Anwendung neu geladen werden.<br/>' +
+                        'Alle ungesicherten Daten gehen dabei verloren.<br/>' +
+                        'Soll die Anwendung jetzt neu geladen werden?', this.reload);
                 }
             }
         }
         this.callParent(arguments);
+    },
+    reload: function(btn) {
+        if (btn === 'yes') {
+            location.reload();
+        }
     }
 });
