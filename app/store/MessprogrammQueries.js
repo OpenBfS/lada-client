fbd2d0b166257790c314464d95dbb970682fb5a6
Raimund Renkert <raimund.renkert@intevation.de>
2015-04-28 10:56:41 +0200

    Added new stores for messprogramm and Probe queries.

---
+++ app/store/MessprogrammQueries.js
@@ -0,0 +1,24 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+
+/**
+ * Store for Queries
+ */
+Ext.define('Lada.store.MessprogrammQueries', {
+    extend: 'Ext.data.Store',
+    model: 'Lada.model.Query',
+    proxy: {
+        type: 'rest',
+        url: 'lada-server/query/messprogramm',
+        reader: {
+            type: 'json',
+            root: 'data'
+        }
+    },
+    autoLoad: true
+});
