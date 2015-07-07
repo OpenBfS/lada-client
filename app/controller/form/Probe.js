4cb33c9f5e8b3035b33b3c5399dfc22371f332bc
Dustin Demuth <dustin@intevation.de>
2015-07-07 09:37:34 +0200

    Added logic to autofill the Netzbetreiber when the Messstelle was selected.

--- app/controller/form/Probe.js
+++ app/controller/form/Probe.js
@@ -29,7 +29,8 @@ Ext.define('Lada.controller.form.Probe', {
             },
             'probeform messstelle combobox':{
                 expand: this.filter,
-                keydown: this.filter
+                keydown: this.filter,
+                select: this.setNetzbetreiber
             },
             'probeform [xtype="datetime"] field': {
                 blur: this.checkDate
@@ -55,6 +56,25 @@ Ext.define('Lada.controller.form.Probe', {
             }
         });
         field.getStore().filter(fil);
+    },
+
+    /**
+     * When a Messtelle is selected, modify the Netzbetreiber
+     * according to the Messstelle
+     * TODO: The conditions when to apply this automatism are still
+     *   unclear. Right now it is only applied when the NB is not set.
+     */
+    setNetzbetreiber: function(combo, records){
+        var netzbetreiber = combo.up().up('form')
+                .down('netzbetreiber').down('combobox');
+        var nbId = records[0].get('netzbetreiberId');
+
+        debugger;
+        if (nbId != null &&
+               (netzbetreiber.value === '' || netzbetreiber.value === null)) {
+            //select the NB in the NB-Combobox
+            netzbetreiber.select(nbId);
+        }
     },
 
     /**
