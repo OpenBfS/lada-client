a88a5146fe6a8b4cceb65f874337c19dbb25194a
Raimund Renkert <rrenkert@intevation.de>
2015-08-14 09:31:46 +0200

    Use rest response handler to act on unauthenticated request.

--- app/override/RestProxy.js
+++ app/override/RestProxy.js
@@ -13,22 +13,22 @@ Ext.define('Lada.override.RestProxy', {
         this.headers = { 'X-OPENID-PARAMS': Lada.openIDParams };
         return this.callParent(arguments);
     },
-
+*/
     processResponse: function (success, operation, request, response, callback, scope) {
         /* Check if we were authenticated at one point (Lada.openIDParams) and
-         * if the response means that we lost that authentcation
-        if (Lada.openIDParams && !success && response.status == 401) {
+         * if the response means that we lost that authentcation */
+        if (!success && response.status == 401) {
             var json = Ext.decode(response.responseText);
             if (json) {
                 if (json.message === "699" || json.message === "698") {
                     /* This is the unauthorized message with the authentication
-                     * redirect in the data 
+                     * redirect in the data */
 
                     /* We decided to handle this with a redirect to the identity
                      * provider. In which case we have no other option then to
-                     * handle it here with relaunch. 
+                     * handle it here with relaunch. */
                     Ext.MessageBox.confirm('Erneutes Login erforderlich',
-                        'Der Server konnte die Anfrage nicht authentifizieren.<br/>'+
+                        'Ihre Session ist abgelaufen.<br/>'+
                         'Für ein erneutes Login muss die Anwendung neu geladen werden.<br/>' +
                         'Alle ungesicherten Daten gehen dabei verloren.<br/>' +
                         'Soll die Anwendung jetzt neu geladen werden?', this.reload);
@@ -37,7 +37,7 @@ Ext.define('Lada.override.RestProxy', {
         }
         this.callParent(arguments);
     },
-*/
+
     reload: function(btn) {
         if (btn === 'yes') {
             location.reload();
