8ec54ebd2b805dee42fbd19522469a2fb672cd06
Tom Gottfried <tom@intevation.de>
2016-05-10 15:32:56 +0200

    Allow reinstalling dependencies and adapt to changed OL-archive content.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR
 curl -O https://cdn.sencha.com/ext/gpl/ext-4.2.1-gpl.zip
 unzip -n ext-4.2.1-gpl.zip
-ln -s ext-4.2.1.883 extjs
+ln -sf ext-4.2.1.883 extjs
 
 mkdir -p resources/lib/ext
 cd resources/lib
@@ -14,17 +14,17 @@ cd resources/lib
 curl -L https://github.com/eligrey/FileSaver.js/archive/master.zip \
      -o FileSaver-js.zip
 unzip -n FileSaver-js.zip
-ln -s FileSaver.js-master FileSaver
+ln -sf FileSaver.js-master FileSaver
 
 curl -L https://github.com/eligrey/Blob.js/archive/master.zip \
      -o Blob-js.zip
 unzip -n Blob-js.zip
-ln -s Blob.js-master Blob
+ln -sf Blob.js-master Blob
 
 curl -L https://github.com/openlayers/openlayers/archive/release-2.13.1.zip \
      -o OpenLayers-2-13-1.zip
 unzip -n OpenLayers-2-13-1.zip
-ln -s openlayers-release-2.13.1/build OpenLayers
+ln -sf ol2-release-2.13.1/build OpenLayers
 cd OpenLayers
 python build.py
 
@@ -32,11 +32,11 @@ cd ../ext
 curl -L https://github.com/elmasse/Ext.i18n.Bundle/archive/v0.3.3.zip \
      -o Ext-i18n-Bundle-v0-3-3.zip
 unzip -n Ext-i18n-Bundle-v0-3-3.zip
-ln -s Ext.i18n.Bundle-0.3.3/i18n i18n
+ln -sf Ext.i18n.Bundle-0.3.3/i18n i18n
 
 curl -L https://github.com/ivan-novakov/extjs-upload-widget/archive/1.1.1.zip \
      -o Ext-ux-Upload-1-1-1.zip
 unzip -n Ext-ux-Upload-1-1-1.zip
-ln -s extjs-upload-widget-1.1.1/lib/upload upload
+ln -sf extjs-upload-widget-1.1.1/lib/upload upload
 
 cd $CUR_DIR
