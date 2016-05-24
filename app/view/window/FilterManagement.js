836d13b9f87bd703c16fddabd0f870c9b7d28b26
Michael Stanko <mstanko@bfs.de>
2016-05-24 15:55:05 +0200

    add messung tab to FilterManagement

--- app/view/window/FilterManagement.js
+++ app/view/window/FilterManagement.js
@@ -35,6 +35,7 @@ Ext.define('Lada.view.window.FilterManagement', {
             handler: this.close
         }];
         var probenstore = Ext.StoreManager.get('probequeries');
+        var messungstore = Ext.StoreManager.get('messungqueries');
         var messpstore = Ext.StoreManager.get('messprogrammqueries');
         var stammstore = Ext.StoreManager.get('stammdatenqueries');
         me.items = [{
@@ -49,6 +50,54 @@ Ext.define('Lada.view.window.FilterManagement', {
                     }
                 },
                 store: probenstore,
+                columns: [{
+                    header: i18n.getMsg('querygrid.header.favorite'),
+                    width: 48,
+                    dataIndex: 'favorite',
+                    xtype: 'favcolumn'
+                }, {
+                    header: i18n.getMsg('querygrid.header.name'),
+                    dataIndex: 'name',
+                    width: 200
+                }, {
+                    header: i18n.getMsg('querygrid.header.description'),
+                    dataIndex: 'description',
+                    minWidth: 200,
+                    flex: 3
+                }, {
+                    header: i18n.getMsg('querygrid.header.columns'),
+                    dataIndex: 'results',
+                    width: 250,
+                    flex: 2,
+                    renderer: function(value) {
+                        var columnString = [];
+                        for (var i = 0; i < value.length; i++) {
+                            columnString.push(value[i].header);
+                        }
+                        return columnString.join(', ');
+                    }
+                }, {
+                    header: i18n.getMsg('querygrid.header.filters'),
+                    dataIndex: 'filters',
+                    width: 200,
+                    renderer: function(value) {
+                        var columnString = [];
+                        for (var i = 0; i < value.length; i++) {
+                            columnString.push(value[i].label);
+                        }
+                        return columnString.join(', ');
+                    }
+                }]
+            }, {
+                title: i18n.getMsg('querygrid.messung.title'),
+                xtype: 'grid',
+                viewConfig: {
+                    markDirty: false,
+                    getRowClass: function() {
+                        return 'x-lada-multiline-grid';
+                    }
+                },
+                store: messungstore,
                 columns: [{
                     header: i18n.getMsg('querygrid.header.favorite'),
                     width: 48,
