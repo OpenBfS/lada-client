7aa1d1720ed5b5eca7cf1cd4b7cbc6f66fcf5993
Tom Gottfried <tom@intevation.de>
2016-10-13 15:35:25 +0200

    Remove debug logging.

--- app/view/widget/DynamicGrid.js
+++ app/view/widget/DynamicGrid.js
@@ -98,8 +98,7 @@ Ext.define('Lada.view.widget.DynamicGrid', {
                 if (rec.get('readonly') === false &&
                     rec.get('owner') === true &&
                     !rec.get('statusEdit')) {
-                    console.log('edit' + rec.get('statusEdit'));
-                        return 'edit';
+                    return 'edit';
                 }
                 else if (rec.get('readonly') === false &&
                     rec.get('owner') === true &&
