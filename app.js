a861a20d9ac817a0bb72c247f65c60c83aa97df1
Marco Lechner, Bundesamt fuer Strahlenschutz, SW 2.1 <mlechner@bfs.de>
2017-04-07 11:31:11 +0200

    set version back to 2.6-SNAPSHOT for new release branch 2.6.x

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ Ext.application({
         Lada.logintime = '';
         Lada.mst = [];
         Lada.netzbetreiber = [];
-        Lada.clientVersion = '2.6.2';
+        Lada.clientVersion = '2.6-SNAPSHOT';
         Lada.serverVersion = '';
 
         var queryString = document.location.href.split('?')[1];
