b8a8139091f7e8a65bfabb9250738e9639fd5bce
Marco Lechner, GeoBoink <marco@marcolechner.de>
2017-05-11 23:11:46 +0200

    set Lada.clientVersion 2.6.3 (using Lada.server 2.6.2)

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ Ext.application({
         Lada.logintime = '';
         Lada.mst = [];
         Lada.netzbetreiber = [];
-        Lada.clientVersion = '2.6-SNAPSHOT';
+        Lada.clientVersion = '2.6.3';
         Lada.serverVersion = '';
 
         var queryString = document.location.href.split('?')[1];
