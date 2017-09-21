9b252fcece00c9e21b49aeb2582dd4d3551d48ba
Maximilian Krambach <mkrambach@intevation.de>
2017-09-21 11:30:12 +0200

    modified build.sh
    installation is shorter now in instructions, but increased in build time

--- build.sh
+++ build.sh
@@ -1,41 +1,18 @@
-# TODO: migration: outdated information!
-
-#Fallback: SENCHAPATH=~/bin/Sencha/Cmd/4.0.5.87/sencha
-if [ -z $SENCHAPATH ]; then SENCHAPATH="~/bin/Sencha/Cmd/4.0.5.87/"; fi
+#Fallback: SENCHAPATH
+if [ -z $SENCHAPATH ]; then SENCHAPATH="~/bin/Sencha/Cmd/6.2.0.103/"; fi
 VNUMBER=$(grep Lada.clientVersion app.js | cut -d '=' -f 2 | cut -d "'" -f 2)
 VERSION=lada-client-$VNUMBER
 
 # Minify
-echo "Minifying...."
-$SENCHAPATH/sencha --sdk-path extjs compile \
-    --classpath=app,resources/lib/ext/upload,resources/lib/ext/i18n,resources/lib/ext/grid,resources/lib/ext/util page \
-    -yui -i index.html -o $VERSION/index.html
+echo "Compiling and minifying...."
+$SENCHAPATH/sencha app build production
 
 # Copy additional files
 # Extjs Styles
 echo "Copying additional files...."
 
-mkdir --parents $VERSION/extjs/resources
-cp -r extjs/resources/css $VERSION/extjs/resources
-cp -r extjs/resources/ext-theme-gray $VERSION/extjs/resources
-
-# Additional resources
-mkdir --parents $VERSION/resources/css
-mkdir --parents $VERSION/resources/i18n
-mkdir --parents $VERSION/resources/img
-mkdir --parents $VERSION/resources/lib
-
-cp -r resources/css $VERSION/resources/
-cp -r resources/img $VERSION/resources/
-cp -r resources/i18n $VERSION/resources/
-cp -r resources/lib/Blob $VERSION/resources/lib/
-cp -r resources/lib/Blob.js-master $VERSION/resources/lib/
-cp -r resources/lib/FileSaver $VERSION/resources/lib/
-cp -r resources/lib/FileSaver.js-master $VERSION/resources/lib/
-cp -r resources/lib/OpenLayers $VERSION/resources/lib/
-cp -r resources/lib/ol2-release-2.13.1 $VERSION/resources/lib/
-
-echo "Compressing...."
-tar -czf $VERSION.tgz $VERSION
+mkdir --parents $VERSION
+mv build/production/Lada build/production/$VERSION
+tar -czf $VERSION.tgz build/production/$VERSION
 
 echo "Done here.\n\n"
