b173da6c2b66a608f71dd432e8fccea0020ecd4e
Maximilian Krambach <mkrambach@intevation.de>
2017-10-09 12:29:47 +0200

    change build process
    remove build/ dir before building, but keep it afterwards
    This avoids the non-minified version not finding it's css in some cases

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ VERSION=lada-client-$VNUMBER
 
 # Minify
 echo "Compiling and minifying...."
-$SENCHAPATH/sencha app build production
+$SENCHAPATH/sencha app build -c production
 
 if [ -d build/production/$VERSION ]; then rm -rf build/production/$VERSION; fi
-mv build/production/Lada build/production/$VERSION
+cp -r build/production/Lada build/production/$VERSION
 tar -czf $VERSION.tgz build/production/$VERSION
 
 echo "Done here.\n\n"
