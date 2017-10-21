aaec9809fac2eee41c79cc506162d26059ee22ba
Marco Lechner, GeoBoink <marco@marcolechner.de>
2017-10-21 10:54:23 +0200

    upgrade OL to 4.4.2; making install-dependencies update friendly

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,32 +2,36 @@
 
 CUR_DIR=`pwd`
 SCRIPT_DIR=`dirname $0`
+OL_VERSION='4.4.2'
+ELMASSE_VERSION='1.2.0'
+FILESAVER_VERSION='master'
+BLOB_VERSION='master'
 
 cd $SCRIPT_DIR
 
 mkdir -p packages/local
 cd packages/local
-curl -L https://github.com/elmasse/elmasse-bundle/archive/v1.2.0.zip \
-    -o elmasse-1.2.0.zip
-unzip -n elmasse-1.2.0.zip
+curl -L https://github.com/elmasse/elmasse-bundle/archive/v${ELMASSE_VERSION}.zip \
+    -o elmasse-${ELMASSE_VERSION}.zip
+unzip -n elmasse-${ELMASSE_VERSION}.zip
 cd ../..
 
 mkdir -p resources/lib
 cd resources/lib
 
-curl -L https://github.com/eligrey/FileSaver.js/archive/master.zip \
+curl -L https://github.com/eligrey/FileSaver.js/archive/${FILESAVER_VERSION}.zip \
      -o FileSaver-js.zip
 unzip -n FileSaver-js.zip
 ln -sf FileSaver.js-master FileSaver
 
-curl -L https://github.com/eligrey/Blob.js/archive/master.zip \
+curl -L https://github.com/eligrey/Blob.js/archive/${BLOB_VERSION}.zip \
      -o Blob-js.zip
 unzip -n Blob-js.zip
 ln -sf Blob.js-master Blob
 
-curl -L https://github.com/openlayers/openlayers/releases/download/v4.3.2/v4.3.2-dist.zip \
-     -o OpenLayers-4.3.2.zip
-unzip -n OpenLayers-4.3.2.zip
-mv v4.3.2-dist ol3/
+curl -L https://github.com/openlayers/openlayers/releases/download/v${OL_VERSION}/v${OL_VERSION}-dist.zip \
+     -o OpenLayers-${OL_VERSION}.zip
+unzip -n OpenLayers-${OL_VERSION}.zip
+mv v${OL_VERSION}-dist ol/
 
 cd $CUR_DIR
