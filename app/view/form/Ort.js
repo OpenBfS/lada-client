8921723568e27db910b01873b9291894c83d22f3
Maximilian Krambach <mkrambach@intevation.de>
2017-11-28 09:50:28 +0100

    change max length of ortId

--- app/view/form/Ort.js
+++ app/view/form/Ort.js
@@ -40,7 +40,7 @@ Ext.define('Lada.view.form.Ort', {
         }, {
             xtype: 'tfield',
             name: 'ortId',
-            maxLength: 10,
+            maxLength: 12,
             labelWidth: 125,
             fieldLabel: i18n.getMsg('orte.ortId')
         }, {
