3bce87f011f02464c80dca79f95bd4276e3bd20e
Michael Stanko <mstanko@bfs.de>
2018-05-29 11:52:29 +0200

    reset version to 3.2-SNAPSHOT (development)

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ Ext.application({
         Lada.logintime = '';
         Lada.mst = [];
         Lada.netzbetreiber = [];
-        Lada.clientVersion = '3.2.1';
+        Lada.clientVersion = '3.2-SNAPSHOT';
         Lada.serverVersion = '';
         // paging sizes available for the client
         Lada.availablePagingSizes = [
