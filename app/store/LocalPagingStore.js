b352f67bde383ab63d4a119f99bf2d162f272774
Maximilian Krambach <mkrambach@intevation.de>
2018-08-13 15:54:13 +0200

    consolidation of an i18n string

--- app/store/LocalPagingStore.js
+++ app/store/LocalPagingStore.js
@@ -98,12 +98,8 @@ Ext.define('Lada.store.LocalPagingStore', {
                 + Math.ceil(count / this.pageSize);
             var low = (this.currentPage - 1) * this.pageSize + 1;
             var high = Math.min(low - 1 + this.pageSize, count);
-            tbs[i].displayMsg = i18n.getMsg('pagingtoolbar.text') + ' '
-                                + low + ' '
-                                + i18n.getMsg('pagingtoolbar.to') + ' '
-                                + high + ' '
-                                + i18n.getMsg('pagingtoolbar.of') + ' '
-                                + count;
+            tbs[i].displayMsg = i18n.getMsg('pagingtoolbar.text', low,
+                high, count);
             tbs[i].onLoad();
             break;
         }
