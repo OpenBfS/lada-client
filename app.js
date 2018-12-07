4b2897fcc0d0b221ca3f08bf9ab03892abf767f5
Michael Stanko <mstanko@bfs.de>
2018-12-07 07:42:03 +0100

    release Version 3.3.1

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ Ext.application({
         Lada.logintime = '';
         Lada.mst = [];
         Lada.netzbetreiber = [];
-        Lada.clientVersion = '3.2-SNAPSHOT';
+        Lada.clientVersion = '3.3.1';
         Lada.serverVersion = '';
         // paging sizes available for the client
         Lada.availablePagingSizes = [
