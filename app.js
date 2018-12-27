78fcce464ff11f4dda36a3e21e9c4ed6e6dcfa70
Marco Lechner, Bundesamt fuer Strahlenschutz, Freiburg, Germany <mlechner@bfs.de>
2018-12-27 10:24:20 +0100

    3.3.3

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ Ext.application({
         Lada.logintime = '';
         Lada.mst = [];
         Lada.netzbetreiber = [];
-        Lada.clientVersion = '3.3-SNAPSHOT';
+        Lada.clientVersion = '3.3.3';
         Lada.serverVersion = '';
         // paging sizes available for the client
         Lada.availablePagingSizes = [
