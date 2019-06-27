227651508502ad8ce33221d3a716d1e8050dd9ac
Alexander Woestmann <awoestmann@intevation.de>
2019-06-27 15:25:59 +0200

    Add logout button to the main panel
    The button starts a logout requests to the shibboleth sp.
    This should trigger the logout after which the application reloads.
    Note that the logout uses multiple redirects which may lead to different origins.
    To prevent browsers from blocking the redirects, the idp must append the proper
    headers to the response.

--- app/controller/Global.js
+++ app/controller/Global.js
@@ -16,6 +16,9 @@ Ext.define('Lada.controller.Global', {
         this.control({
             'button[action=about]': {
                 click: this.about
+            },
+            'button[action=logout]': {
+                click: this.logout
             }
         });
     },
@@ -26,5 +29,19 @@ Ext.define('Lada.controller.Global', {
     about: function() {
         var win = Ext.create('Lada.view.window.About');
         win.show();
+    },
+
+    logout: function() {
+        var i18n = Lada.getApplication().bundle;
+        Ext.Ajax.setUseDefaultXhrHeader(false);
+        Ext.Ajax.request({
+            url: '/Shibboleth.sso/Logout',
+            success: function (response) {
+                window.location.reload();
+            },
+            failure: function (response) {
+                Ext.Msg.alert(i18n.getMsg('err.msg.slo.failed.title'), i18n.getMsg('err.msg.slo.failed.body'));
+            }
+        })
     }
 });
