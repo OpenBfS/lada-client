043bee9fc02e505812a2265a18c83361f8a15002
Alexander Woestmann <awoestmann@intevation.de>
2019-08-02 15:48:35 +0200

    Add window tracker and tracked window class
    Both classes are used to prevent multiple windows for the same record
    from being shown. See WindowTracker source file for usage information.

---
+++ app/view/window/TrackedWindow.js
@@ -0,0 +1,39 @@
+
+
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+
+/**
+ * Base window class which overrides show and close functions to use the WindowTracker functions.
+ * Overwrite recordType to make use of the tracking.
+ */
+Ext.define('Lada.view.window.TrackedWindow', {
+    extend: 'Ext.window.Window',
+
+    recordType: null,
+
+    /**
+     * Checks if a window for the current record is already open.
+     * If so, the already exisiting window will be focus, else
+     * this window will be shown.
+     */
+    show: function() {
+        if (Lada.util.WindowTracker.isOpen(this.recordType, this.record.get('id'))) {
+            Lada.util.WindowTracker.focus(this.recordType, this.record.get('id'));
+        } else {
+            Lada.util.WindowTracker.open(this.recordType, this.record.get('id'), this);
+            this.callParent();
+        }
+    },
+
+
+    close: function() {
+        Lada.util.WindowTracker.close(this.recordType, this.record.get('id'), this);
+        this.callParent();
+    }
+});
