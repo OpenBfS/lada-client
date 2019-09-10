6d20d92ebb9a1e052e23c10501397a6265bc2e8e
Alexander Woestmann <awoestmann@intevation.de>
2019-09-10 14:52:42 +0200

    Add expired session handling to rest proxy
    The client now recognizes an expired shibboleth sp session by receiving an
    html form as a result of a lada server query.
    If a form is received the client follows the redirects until the lada server
    endpoint is reached. The sp session is then renewed.
    
    Note: During this process, the initial requests JSON data is dropped.
    If the initial request is a POST, PUT or DELETE request requiring payload,
    it is likely to fail and should be repeated by the user.
    GET requests should be successful as the URL parameters get preserved during
    the process.

--- app/override/RestProxy.js
+++ app/override/RestProxy.js
@@ -12,35 +12,36 @@ Ext.define('Lada.override.RestProxy', {
     processResponse: function(success, operation, request, response, callback, scope) {
         //SSO will send an html form if session is expired. Check content type for json or html:
         var me = this;
+        var parent = this.callParent;
         var contentType = response.getAllResponseHeaders()['content-type'];
         if (contentType !== 'application/json') {
             var i18n = Lada.getApplication().bundle;
-            Ext.MessageBox.confirm(
-                i18n.getMsg('err.msg.sso.expired.title'),
-                i18n.getMsg('err.msg.sso.expired.body'),
-                function(btn) {
-                    me.reload(btn, response, '/lada-idp/idp/profile/SAML2/POST/SSO');
-                }
-            );
+            try {
+                Ext.MessageBox.confirm(
+                    i18n.getMsg('err.msg.sso.expired.title'),
+                    i18n.getMsg('err.msg.sso.expired.body'),
+                    function(btn) {
+                        me.reload(btn, response, request).then(function(args) {
+                            var succ = args[1];
+                            var resp = args[2];
+                            me.processResponse(succ, operation, request, resp, callback, scope);
+                            Ext.MessageBox.alert({
+                                title: i18n.getMsg('shib.session.renewed.title'),
+                                message: i18n.getMsg('shib.session.renewed.text')
+                            });
+                        });
+                    }
+                );
+            } catch (e) {
+                Ext.MessageBox.alert({
+                    title: i18n.getMsg('err.msg.sso.renew.failed.title'),
+                    message: i18n.getMsg('err.msg.sso.renew.failed.body')
+                });
+            }
+            
         } else {
             this.callParent(arguments);
         }
-
-        /*
-           SSO will send a 302 if the Client is not authenticated
-           unfortunately this seems to be filtered by the browser.
-           We assume that a 302 was send when the follwing statement
-           is true.
-        */
-        /*
-        if (!success && response.status === 0 && response.responseText === '') {
-            var i18n = Lada.getApplication().bundle;
-            Ext.MessageBox.confirm(
-                i18n.getMsg('err.msg.sso.expired.title'),
-                i18n.getMsg('err.msg.sso.expired.body'),
-                this.reload
-            );
-        } */
     },
 
     /**
@@ -70,53 +71,141 @@ Ext.define('Lada.override.RestProxy', {
         console.log(status);
     },
 
-    reload: function(btn, response, url) {
-        var me = this;
-        if (btn === 'yes') {
-            //Parse responseform to get saml request parameter
-            var response = response.responseText;
-            var parser = new DOMParser();
-            var xmlDoc = parser.parseFromString(response,"text/html");
-            //Parameters can be found as value of hidden input fields
-            var inputFields = xmlDoc.getElementsByTagName('input');
-            var relayState = inputFields.RelayState.value;
-            var samlRequest = inputFields.SAMLRequest.value;
-            var idpUrl = xmlDoc.getElementsByTagName('form')[0].action;
-
-            //Issue a request to the idp containing the form data
-            Ext.Ajax.setUseDefaultXhrHeader(false);
-            Ext.Ajax.setWithCredentials(true);
-            Ext.Ajax.request({
-                url: url,
-                method: 'POST',
-                params: {
-                    RelayState: relayState,
-                    SAMLRequest: samlRequest
-                },
-                callback: function(opts, success, idpresponse) {
-                    //Parse responseform to get sso request parameter
-                    var idpresponseText = idpresponse.responseText;
-                    var parser = new DOMParser();
-                    var xmlDoc = parser.parseFromString(idpresponse,"text/html");
-                    //Parameters can be found as value of hidden input fields
-                    var inputFields = xmlDoc.getElementsByTagName('input');
-                    var relayState = inputFields.RelayState.value;
-                    var samlRequest = inputFields.SAMLRequest.value;
-                    Ext.Ajax.setUseDefaultXhrHeader(false);
-                    Ext.Ajax.setWithCredentials(true);
-                    Ext.Ajax.request({
-                        url: '/Shibboleth.sso/SAML2/POST',
-                        method: 'POST',
-                        jsonData: {
-                            RelayState: relayState,
-                            SAMLRequest: samlRequest
-                        },
-                        callback: function(opts, success, idpresponse) {
-                            debugger;
-                        }
-                    });
-                }
-            });
+    /**
+     * Parse the form inside an idp response and send
+     * the response containing form data
+     */
+    parseAuthenticationRequest: function(responseObj, callback) {
+        //Parse responseform to get saml request parameter
+        //This should be the response to the request to the lada server
+        var response = responseObj.responseText;
+        var parser = new DOMParser();
+        var xmlDoc = parser.parseFromString(response,"text/html");
+        //Parameters can be found as value of hidden input fields
+        var idpUrl = this.getActionUrl(responseObj);
+        var inputFields = xmlDoc.getElementsByTagName('input');
+        var params = {};
+        for (var i = 0; i < inputFields.length; i++) {
+            var field = inputFields[i];
+            var name = field.getAttribute('name');
+            var value = field.getAttribute('value');
+            if (name && name != "") {
+                params[name] = value;
+            }
         }
+        //Issue a request to the idp containing the form data
+        Ext.Ajax.setUseDefaultXhrHeader(false);
+        Ext.Ajax.setWithCredentials(true);
+        Ext.Ajax.request({
+            url: idpUrl,
+            method: 'POST',
+            params: params,
+            callback: callback ? callback : null
+        });
+    },
+
+    /**
+     * Return the action url of a form tag inside a response
+     */
+    getActionUrl: function(responseObj) {
+        var response = responseObj.responseText;
+        var parser = new DOMParser();
+        var xmlDoc = parser.parseFromString(response,"text/html");
+        return xmlDoc.getElementsByTagName('form')[0].getAttribute('action');
+
+    },
+
+    /**
+     * Renew an expired shibboleth sp session.
+     */
+    reload: function(btn, response, initialRequest) {
+        var me = this;
+        return new Ext.Promise(function(resolve, reject) {
+            Ext.Ajax.on("beforerequest",function(con){
+                con.setUseDefaultXhrHeader(false);
+                con.setWithCredentials(true);
+              });
+            if (btn === 'yes') {
+                //Get the idp base url
+                var idpBaseUrl = me.getActionUrl(response).match(/^https:\/\/[^\/]+/i);
+                //Parse the first response form and send data back to idp
+                me.parseAuthenticationRequest(response, function(opts, success, idpresponse) {
+                        /* The response should be a form and scripts to save session
+                        information to the local storage */
+                        var idpresponseText = idpresponse.responseText;
+                        var parser = new DOMParser();
+                        var xmlDoc = parser.parseFromString(idpresponseText,"text/html");
+                        //Parameters can be found as value of hidden input fields
+                        var inputFields = xmlDoc.getElementsByTagName('input');
+                        var params = {};
+                        for (var i = 0; i < inputFields.length; i++) {
+                            var field = inputFields[i];
+                            var name = field.getAttribute('name');
+                            var value = field.getAttribute('value');
+                            if (name && name != "") {
+                                params[name] = value;
+                            }
+                        }
+                        var execUrl = idpBaseUrl
+                                + xmlDoc.getElementsByTagName('form')[0].getAttribute('action');
+                        var scripts = xmlDoc.getElementsByTagName('script');
+                        var wrappingDiv = xmlDoc.getElementsByClassName('wrapper')[0];
+    
+                        var scripts = xmlDoc.getElementsByTagName('script');
+                        var infoScript;
+                        for (var j = 0; j < scripts.length; j++) {
+                            var tag = scripts[j]
+                            if (tag.parentElement.className === 'container') {
+                                infoScript = tag;
+                            }
+                        }
+                        writeLocalStorage = function(key, value) {
+                            params = me.writeLocalStorage(key, value, params);
+                        }
+                        var funcText = infoScript.innerText;
+                        funcText = funcText.replace('<!--', '');
+                        funcText = funcText.replace('-->', '');
+                        funcText = funcText.replace('function doSave() {', '');
+                        funcText = funcText.replace('document.form1.submit()', '');
+                        funcText = funcText.replace('}', '');
+                        var doSave = new Function(funcText);
+                        doSave.call();
+                        //Issue the last request to the idp
+                        Ext.Ajax.request({
+                            url: execUrl,
+                            method: 'POST',
+                            params: params,
+                            callback: function(opts, success, idpresponse) {
+                                /* Parse saml response parameters and send to sp.
+                                   This request should be redirected to the actual rest service */
+                                me.parseAuthenticationRequest(idpresponse, function() {
+                                    resolve(arguments);
+                                })
+                            }
+                        });
+                    }
+                );
+            }
+        });
+    },
+
+    /**
+     * Write session info to local storage and set params accordingly
+     */
+    writeLocalStorage: function(key, value, params) {
+        var success;
+        try {
+            if (value == null || value.length == 0) {
+                localStorage.removeItem(key);
+            } else {
+                localStorage.setItem(key, value);
+            }
+            success = "true";
+        } catch (e) {
+            success = "false";
+            params["shib_idp_ls_exception." + key] = e;
+        }
+        params["shib_idp_ls_success." + key] = success;
+        return params;
     }
 });
