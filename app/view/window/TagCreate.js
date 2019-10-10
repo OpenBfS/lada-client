08ecd8fc41c84f569c59b411f7eb66fd715e8739
Alexander Woestmann <awoestmann@intevation.de>
2019-10-10 14:24:46 +0200

    Add functions to create and remove tags from a grid selection of probe items
    * Renames the TagAssign window to TagEdit window
    * Adds a "Remove Tag" button to the TagEdit window which removes the chosen tags
      from the grid selection
    * Adds a "Create Tag" button to the TagEdit window which opens a tag creation window.
      After saving the new tag it is assigned to the probe grid selection.

--- app/view/window/TagCreate.js
+++ app/view/window/TagCreate.js
@@ -15,9 +15,31 @@ Ext.define('Lada.view.window.TagCreate', {
 
     layout: 'vbox',
     width: 300,
+
+    /**
+     * Tagwidget associated with this create window
+     */
     tagWidget: null,
-    //A Probe's hauptprobenummer, used in the window title
+
+    /**
+     * Tag edit window associated with this create window
+     */
+    tagEdit: null,
+    /**
+     * Probe instance set if creating a tag for a single probe
+     */
     probe: null,
+
+    /**
+     * Array of probe instances used if creating tags for a selection
+     */
+    selection: [],
+
+    /**
+     * Mode. Can either be 'single' for creating a tag for a single probe
+     * or 'bulk' for creating a tag for a selection
+     */
+    mode: 'single',
 
     /**
      * This function initialises the Window
@@ -25,7 +47,9 @@ Ext.define('Lada.view.window.TagCreate', {
     initComponent: function() {
         var i18n = Lada.getApplication().bundle;
         var me = this;
-        this.title = i18n.getMsg('title.tagcreatewindow', this.probe);
+        this.title = this.mode === 'single' ?
+                i18n.getMsg('title.tagcreatewindowprobe', this.probe):
+                i18n.getMsg('title.tagcreatewindowbulk', this.selection.length);
         this.items = [{
             xtype: 'textfield',
             width: '100%',
@@ -54,21 +78,75 @@ Ext.define('Lada.view.window.TagCreate', {
     },
 
     /**
+     * Saves a tag for a single probe instance
+     */
+    saveSingleTag: function(textfield) {
+        var text = textfield.getValue();
+        this.tagWidget.createTag(text);
+        textfield.reset();
+        Ext.getCmp('dynamicgridid').reload();
+        var tagFilterWidget = Ext.getCmp('tagfilterwidget');
+        if (tagFilterWidget) {
+            tagFilterWidget.reload(true);
+        }
+    },
+
+    /**
+     * Creates and saves a tag for a selection of probe instances.
+     * As tags are only created if they are associated with a probe,
+     * the first step is to create a tag for the first selected probe.
+     * Then it is chosen in the tag widget combobox and saved via the TagEdit window.
+     */
+    saveBulkTag: function(textfield) {
+        var me = this;
+        if (!this.selection || this.selection.length == 0) {
+            return;
+        }
+        //Get the first probeId
+        var firstPid = this.selection[0].data.probeId;
+        if (!firstPid) {
+            return;
+        }
+        var text = textfield.getValue();
+
+        //Save tag for first probe. Should trigger itemadd event on tag widget.
+        this.tagWidget.store.createTagForPid(text, firstPid, function(response) {
+            var oldItems = Ext.clone(me.tagWidget.store.getData().items);
+            //Wait for the reload to finish
+            me.tagWidget.reload(true, function() {
+                var newItems = Ext.clone(me.tagWidget.store.getData().items);
+                //Look for the new item
+                var newItem = null;
+                for (var i = 0; i < newItems.length; i++) {
+                    var id = newItems[i].id;
+                    if (!Ext.Array.findBy(oldItems,
+                            function(item, index) {
+                                return id == item.id;
+                            })
+                    ) {
+                        newItem = newItems[i];
+                        break;
+                    }
+                }
+                //Select new item in combobox and fire click event
+                me.tagWidget.clearValue();
+                me.tagWidget.select(newItem);
+                me.tagEdit.down('button[action=bulkaddtags]').click();
+            });
+            me.close();
+        });
+    },
+
+    /**
      * Handles click on save button:
-     * Validate textfield input, if valid create tag and clear textfield
+     * Validate textfield input, if valid call single or bulk create function
      */
     handleSaveClicked: function(button) {
         var me = this.up();
         var textfield = me.down('textfield');
         var text = textfield.getValue();
         if (textfield.validate()) {
-            me.tagWidget.createTag(text);
-            textfield.reset();
-            Ext.getCmp('dynamicgridid').reload();
-            var tagFilterWidget = Ext.getCmp('tagfilterwidget');
-            if (tagFilterWidget) {
-                tagFilterWidget.reload(true);
-            }
+            me.mode === 'single' ? me.saveSingleTag(textfield): me.saveBulkTag(textfield);
         }
     }
 });
---
+++ app/view/window/TagEdit.js
@@ -0,0 +1,173 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+
+/**
+ * Window for assigning tags to multiple probe items.
+ */
+Ext.define('Lada.view.window.TagEdit', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.tageditwindow',
+
+    layout: 'vbox',
+    width: 400,
+    selection: null,
+
+
+    /**
+     * This function initialises the Window
+     */
+    initComponent: function() {
+        var i18n = Lada.getApplication().bundle;
+        var me = this;
+        this.items = [{
+            xtype: 'container',
+            layout: 'hbox',
+            width: '100%',
+            name: 'tagwidgetcontainer',
+            items: [{
+                xtype: 'tagwidget',
+                margin: '5 5 5 5',
+                width: '75%',
+                mode: 'bulk'
+            }, {
+                width: 25,
+                height: 25,
+                xtype: 'button',
+                margin: '5 5 5 0',
+                action: 'createtag',
+                icon: 'resources/img/list-add.png',
+                tooltip: i18n.getMsg('button.createtag.tooltip'),
+                handler: function(button) {
+                    var win = Ext.create('Lada.view.window.TagCreate', {
+                        tagWidget: me.down('tagwidget'),
+                        mode: 'bulk',
+                        tagEdit: me,
+                        selection: me.selection,
+                        probe: null
+                    });
+                    //Close window if parent window is closed
+                    me.on('close', function() {
+                        win.close();
+                    })
+                    win.show();
+                }
+            }]
+        }, {
+            xtype: 'container',
+            layout: 'hbox',
+            name: 'buttoncontainer',
+            items: [{
+                xtype: 'button',
+                action: 'bulkaddtags',
+                margin: '5 5 5 5',
+                text: i18n.getMsg('tag.assignwindow.assignbutton.text'),
+                handler: this.assignToSelection
+            },{
+                xtype: 'button',
+                action: 'bulkdeletetags',
+                margin: '5 5 5 5',
+                text: i18n.getMsg('tag.assignwindow.unassignbutton.text'),
+                handler: this.unassignFromSelection
+            },{
+                xtype: 'button',
+                text: i18n.getMsg('cancel'),
+                margin: '5 5 5 5',
+                handler: function() {
+                    me.close();
+                }
+            }]
+        }, {
+            xtype: 'progressbar',
+            hidden: true,
+            width: '100%',
+            margin: '5 10 10 10'
+        }];
+        this.callParent(arguments);
+    },
+
+    /**
+     * Eventhandler that unassigns the selected tag(s) from the selected probe instance(s)
+     * @param button Button that caused the event
+     */
+    unassignFromSelection: function(button) {
+        var me = button.up('tageditwindow');
+        var i18n = Lada.getApplication().bundle;
+        var tagwidget = me.down('tagwidget');
+        var tags = tagwidget.getValue();
+        var store = Ext.create('Lada.store.Tag', {
+            autoload: false
+        });
+        var tagCount = tags.length * me.selection.length;
+        var tagsSet = 0;
+        me.down('progressbar').updateProgress(0, i18n.getMsg('tag.assignwindow.progress', 0, tagCount, false));
+        me.down('progressbar').show();
+        me.down('container[name=buttoncontainer]').hide();
+        me.down('tagwidget').disable();
+        me.down('button[action=createtag]').disable();
+
+        for (var i = 0; i < me.selection.length; i++) {
+            var probeId = me.selection[i].data.probeId;
+            store.pId = probeId;
+            for (var j = 0; j < tags.length; j++) {
+                var tag = tags[j];
+                store.deleteZuordnung(tag, function(request, success, response) {
+                    var responseJson = Ext.decode(response.responseText);
+                    tagsSet++;
+                    var ratio = tagsSet/tagCount;
+                    me.down('progressbar').updateProgress(
+                            ratio, i18n.getMsg('tag.assignwindow.progress', tagsSet, tagCount, false));
+                    if (ratio == 1) {
+                        Ext.getCmp('dynamicgridid').reload();
+                    }
+                });
+            }
+        }
+    },
+
+    /**
+     * Eventhandler that assigns the selected tag(s) to the chosen probe instance(s)
+     * @param button Button that caused the event
+     */
+    assignToSelection: function(button) {
+        var me = button.up('tageditwindow');
+        var i18n = Lada.getApplication().bundle;
+        var tagwidget = me.down('tagwidget');
+        var tags = tagwidget.getValue();
+        var store = Ext.create('Lada.store.Tag', {
+            autoload: false
+        });
+        var tagCount = tags.length * me.selection.length;
+        var tagsSet = 0;
+        me.down('progressbar').updateProgress(0, i18n.getMsg('tag.assignwindow.progress', 0, tagCount, false));
+        me.down('progressbar').show();
+        me.down('container[name=buttoncontainer]').hide();
+        me.down('tagwidget').disable();
+        me.down('button[action=createtag]').disable();
+
+        for (var i = 0; i < me.selection.length; i++) {
+            var probeId = me.selection[i].data.probeId;
+            store.pId = probeId;
+            for (var j = 0; j < tags.length; j++) {
+                var tag = tags[j];
+                store.createZuordnung(tag, function(request, success, response) {
+                    var responseJson = Ext.decode(response.responseText);
+                    if (success == false) {
+                        console.log('500 on tag set');
+                    }
+                    tagsSet++;
+                    var ratio = tagsSet/tagCount;
+                    me.down('progressbar').updateProgress(
+                            ratio, i18n.getMsg('tag.assignwindow.progress', tagsSet, tagCount, false));
+                    if (ratio == 1) {
+                        Ext.getCmp('dynamicgridid').reload();
+                    }
+                });
+            }
+        }
+    }
+});
