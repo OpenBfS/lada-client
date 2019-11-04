386b06b5e2c2df6b52185eae6c88a59bd1d3c4c0
Evi Huber <ehuber@bfs.de>
2019-11-04 13:49:20 +0100

    add error new codes and fix typo

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ Ext.application({
         Lada.logintime = '';
         Lada.mst = [];
         Lada.netzbetreiber = [];
-        Lada.clientVersion = '3.3.14-SNAPSHOT';
+        Lada.clientVersion = '3.3.13.1';
         Lada.serverVersion = '';
         // paging sizes available for the client
         Lada.availablePagingSizes = [
