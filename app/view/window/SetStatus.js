e5ea1b6952319067d53dbdf7d30c42fd777b5929
Evi Huber <ehuber@bfs.de>
2019-11-18 09:51:45 +0100

    display error/warnings setting status from messungsList

--- app/view/window/SetStatus.js
+++ app/view/window/SetStatus.js
@@ -23,6 +23,7 @@ Ext.define('Lada.view.window.SetStatus', {
     record: null,
 
     modal: true,
+    constrain: true,
     closable: false,
     resultMessage: '',
 
@@ -189,10 +190,39 @@ Ext.define('Lada.view.window.SetStatus', {
                     jsonData: data,
                     success: function(response) {
                         var json = Ext.JSON.decode(response.responseText);
-                        me.resultMessage += '<strong>' + i18n.getMsg('messung') + ': ';
                         var sel = me.selection[count];
-                        me.resultMessage += sel.get('hpNr') + ' - ' + sel.get('npNr') + '</strong><br><dd>';
-                        me.resultMessage += i18n.getMsg('status-' + json.message) + '</dd><br>';
+                        var errors = json.errors;
+                        var out = [];
+                        var numErrors;
+                        if (!Ext.isObject(errors)) {
+                            numErrors = 0;
+                        } else {
+                            numErrors = Object.keys(errors).length;
+                        }
+                        if (numErrors > 0) {
+                            var msgs;
+                            out.push('<dd><ul>');
+                            for (var key in errors) {
+                                msgs = errors[key];
+                                var validation = [];
+                                for (var j = msgs.length - 1; j >= 0; j--) {
+                                    validation.push('<li><b>' + i18n.getMsg(key) + ':</b> ' + i18n.getMsg(msgs[j].toString()) + '</li>');
+                                }
+                                out.push(validation.join(''));
+                            }
+                            out.push('</ul></dd>');
+                        }
+                        out.push('<hr>');
+                        for ( z=0; z < me.selection.length; z++) {
+                            if (me.selection[z].get('id') == json.data.messungsId) {
+                                me.resultMessage += '<strong>' + i18n.getMsg('hauptprobenNr') + ' - ' + i18n.getMsg('nebenprobenNr') + ': </strong>';
+                                me.resultMessage += me.selection[z].get('hpNr') || '<i>HP-Nr. nicht vergeben</i>';
+                                me.resultMessage += ' - ';
+                                me.resultMessage += me.selection[z].get('npNr') || '<i><i>NP-Nr. nicht vergeben</i></i>';
+                                me.resultMessage += '<dl><dd>' + i18n.getMsg('status-' + json.message) + '</dd></dl>';
+                            }
+                        }
+                        me.resultMessage += out.join('');
                         count++;
                         progress.updateProgress(count / me.selection.length, progressText + ' (' + count + ')');
                         if (count === me.selection.length) {
@@ -236,11 +266,11 @@ Ext.define('Lada.view.window.SetStatus', {
 
                         var probenform = Ext.ComponentQuery.query('probeform');
                         var hauptprobennummer = probenform[0].getRecord().get('hauptprobenNr');
-                        me.resultMessage += '<strong>' + i18n.getMsg('hauptprobenNr') + ' - ' + i18n.getMsg('nebenprobenNr') + ': ';
-                        me.resultMessage += hauptprobennummer || '';
+                        me.resultMessage += '<strong>' + i18n.getMsg('hauptprobenNr') + ' - ' + i18n.getMsg('nebenprobenNr') + ': </strong>';
+                        me.resultMessage += hauptprobennummer || '<i>HP-Nr. nicht vergeben</i>';
                         me.resultMessage += ' - ';
-                        me.resultMessage +=  me.record.get('nebenprobenNr') || '';
-                        me.resultMessage += '</strong><br><dd>' + i18n.getMsg('status-' + json.message) + '</dd><br>';
+                        me.resultMessage +=  me.record.get('nebenprobenNr') || '<i>NP-Nr. nicht vergeben</i><br>';
+                        me.resultMessage += '<dl><dd>' + i18n.getMsg('status-' + json.message) + '</dd></dl>';
                         progress.updateProgress(1, progressText + ' (' + 1 + ')');
                         var errors = json.errors;
                         var out = [];
