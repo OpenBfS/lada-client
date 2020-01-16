5c24aaa23ae2363cffeb3cb56e20cc2c963ac11a
Michael Stanko <mstanko@bfs.de>
2020-01-16 07:12:56 +0100

    set version to 3.3.16-SNAPSHOT

--- app.js
+++ app.js
@@ -155,7 +155,7 @@ Ext.application({
         Lada.logintime = '';
         Lada.mst = [];
         Lada.netzbetreiber = [];
-        Lada.clientVersion = '3.3.15-SNAPSHOT';
+        Lada.clientVersion = '3.3.16-SNAPSHOT';
         Lada.serverVersion = '';
         // paging sizes available for the client
         Lada.availablePagingSizes = [
