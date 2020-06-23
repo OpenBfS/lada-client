74663a14a9907f4bad6e5eaa3075c9b36a90a259
Michael Stanko <mstanko@bfs.de>
2020-06-23 13:20:46 +0200

    make install dependencies a bitmore quiet and faster

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -11,33 +11,33 @@ cd $SCRIPT_DIR
 
 mkdir -p packages/local
 cd packages/local
-curl -L https://github.com/elmasse/elmasse-bundle/archive/v${ELMASSE_VERSION}.zip \
-    -o elmasse-${ELMASSE_VERSION}.zip
-unzip -n elmasse-${ELMASSE_VERSION}.zip
+curl -sSL https://github.com/elmasse/elmasse-bundle/archive/v${ELMASSE_VERSION}.zip \
+     -o elmasse-${ELMASSE_VERSION}.zip
+unzip -q -n elmasse-${ELMASSE_VERSION}.zip
 cd ../..
 
 mkdir -p resources/lib
 cd resources/lib
 
-curl -L https://github.com/eligrey/FileSaver.js/archive/${FILESAVER_VERSION}.zip \
+curl -sSL https://github.com/eligrey/FileSaver.js/archive/${FILESAVER_VERSION}.zip \
      -o FileSaver-js-${FILESAVER_VERSION}.zip
-unzip -n FileSaver-js-${FILESAVER_VERSION}.zip
+unzip -q -n FileSaver-js-${FILESAVER_VERSION}.zip
 rm -rf FileSaver
 ln -sf FileSaver.js-${FILESAVER_VERSION} FileSaver
 
-curl -L https://github.com/eligrey/Blob.js/archive/${BLOB_VERSION}.zip \
+curl -sSL https://github.com/eligrey/Blob.js/archive/${BLOB_VERSION}.zip \
      -o Blob-js-${BLOB_VERSION}.zip
-unzip -n Blob-js-${BLOB_VERSION}.zip
+unzip -q -n Blob-js-${BLOB_VERSION}.zip
 rm -rf Blob 
 ln -sf Blob.js-${BLOB_VERSION} Blob
 
-curl -L https://github.com/openlayers/openlayers/releases/download/v${OL_VERSION}/v${OL_VERSION}-dist.zip \
+curl -sSL https://github.com/openlayers/openlayers/releases/download/v${OL_VERSION}/v${OL_VERSION}-dist.zip \
      -o OpenLayers-${OL_VERSION}.zip
-unzip -n OpenLayers-${OL_VERSION}.zip
+unzip -q -n OpenLayers-${OL_VERSION}.zip
 rm -rf ol
 mv v${OL_VERSION}-dist ol/
 
-curl -L https://momentjs.com/downloads/moment.js -o moment.js
-curl -L https://momentjs.com/downloads/moment-timezone-with-data.js -o moment-timezone.js
+curl -sSL https://momentjs.com/downloads/moment.js -o moment.js
+curl -sSL https://momentjs.com/downloads/moment-timezone-with-data.js -o moment-timezone.js
 
 cd $CUR_DIR
