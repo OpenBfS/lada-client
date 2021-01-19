f43daa3667796d274e9f525d3592d1388cbb3c6b
Tom Gottfried <Tom Gottfried tom@intevation.de>
2021-01-19 16:24:37 +0100

    Shrink size of Docker image by about half a gigabyte
    Remove downloaded zip files after unpacking and partly remove
    unused content.

--- install-sencha2opt.sh
+++ install-sencha2opt.sh
@@ -5,20 +5,24 @@ WORKSPACE=$CUR_DIR
 SENCHA_CMD_VERSION="7.2.0.84"
 EXTJS_VERSION="6.2.0"
 
-if [ ! -e SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip ]
+if [ ! -e SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh ]
 then
   curl -sSL http://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/no-jre/SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip \
      -o SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
+  unzip -q -n SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
+  rm SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
 fi
-unzip -q -n SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
 ./SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh -q -dir "/opt/$SENCHA_CMD_VERSION"
 
-if [ ! -e ext-$EXTJS_VERSION-gpl.zip ]
-then 
+if [ ! -e ext-$EXTJS_VERSION ]
+then
   curl -sSL  http://cdn.sencha.com/ext/gpl/ext-$EXTJS_VERSION-gpl.zip \
      -o ext-$EXTJS_VERSION-gpl.zip
+  unzip -q -n ext-$EXTJS_VERSION-gpl.zip
+  rm ext-$EXTJS_VERSION-gpl.zip
+  # Examples not needed but use a lot of disk space
+  find ext-$EXTJS_VERSION -name examples -type d -print0 | xargs -0 rm -r
 fi
-unzip -q -n ext-$EXTJS_VERSION-gpl.zip
 
 ln -s /opt/$SENCHA_CMD_VERSION /opt/Sencha
 
