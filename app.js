6d42f60e6c5aba692eb17d06e39ae74e7b28c568
Michael Stanko <mstanko@bfs.de>
2021-08-26 16:20:59 +0200

    4.3.3

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ Ext.application({
         Lada.logintime = '';
         Lada.mst = [];
         Lada.netzbetreiber = [];
-        Lada.clientVersion = '4.3.1';
+        Lada.clientVersion = '4.3.3';
         Lada.serverVersion = '';
         // paging sizes available for the client
         Lada.availablePagingSizes = [
