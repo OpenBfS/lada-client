b87b74a1044b2d161c3d70e5dc89a4ccb3905699
Evi Huber <ehuber@bfs.de>
2022-02-09 11:38:12 +0100

    sorting store kerntechn. Anlage(n) by ktaGruppe not id

--- app/store/KtaGruppe.js
+++ app/store/KtaGruppe.js
@@ -14,7 +14,7 @@ Ext.define('Lada.store.KtaGruppe', {
     model: 'Lada.model.KtaGruppe',
     sorters: [
         {
-            property: 'id',
+            property: 'ktaGruppe',
             direction: 'ASC'
         }],
     autoLoad: true,
