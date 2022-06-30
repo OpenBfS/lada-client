49107e8e9f3134c5a1d8b0a4f6563363d2ef3f8a
Tom Gottfried <Tom Gottfried tom@intevation.de>
2022-06-30 19:37:33 +0200

    Fixup rev. ef86189de5fa1c7686d11a53c02c592ec122ba93
    Keeping the config at the panel leads to forms considering the
    panel as a form field and calling methods on it (like validate())
    that it does not have. This problem does not seem to occur with
    Lada.view.widget.base.ComboBox, maybe because it extends
    Ext.form.Panel, while here Ext.panel.Panel is extended
    (accidentally?).

--- app/view/widget/base/TextField.js
+++ app/view/widget/base/TextField.js
@@ -18,7 +18,6 @@ Ext.define('Lada.view.widget.base.TextField', {
     border: false,
     margin: '0, 0, 5, 0',
 
-    isFormField: true,
     textFieldCls: '',
     triggers: null,
 
@@ -39,7 +38,9 @@ Ext.define('Lada.view.widget.base.TextField', {
             enforceMaxLength: this.enforceMaxLength || true,
             fieldLabel: this.fieldLabel,
             labelWidth: this.labelWidth,
-            isFormField: this.isFormField,
+            isFormField: this.isFormField === undefined
+                ? true
+                : this.isFormField,
             readOnly: this.readOnly || false,
             validator: this.validator,
             listeners: this.listeners,
