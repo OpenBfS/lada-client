b7e71ff3b21585a8039140914dd0ac9a5a55ab75
Tom Gottfried <Tom Gottfried tom@intevation.de>
2022-07-28 19:46:07 +0200

    Implement setting Messstelle/Labor combination in single widget
    instead of code spread over app.js, forms, controllers and windows with
    lots of duplication.

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ Ext.application({
         'Lada.store.Messgroessen',
         'Lada.store.Messstellen',
         'Lada.store.Leitstelle',
-        'Lada.store.MessstellenKombi',
         'Lada.store.Probenarten',
         'Lada.store.Probenzusaetze',
         'Lada.store.Staaten',
@@ -55,7 +54,6 @@ Ext.application({
         'Lada.store.MessprogrammKategorie',
         'Lada.store.GridColumn',
         'Lada.store.Query',
-        'Lada.model.MessstelleLabor',
         'Lada.model.Messstelle',
         'Lada.model.GenericResults',
         'Lada.model.GridColumn',
@@ -225,24 +223,6 @@ Ext.application({
             Lada.mst.push(mstLabor[i].labor);
         }
 
-        // Used for widget.MessstelleLabor in form.Probe and
-        // respective windows and controller
-        var mstLaborStore = Ext.create('Ext.data.Store', {
-            storeId: 'messstellelabor',
-            // Model without proxy. Data added in load-callback
-            // on store 'messstellen' created further down.
-            model: 'Lada.model.MessstelleLabor'
-        });
-
-        // Used for widget.MessstelleLabor in form.Messprogramm,
-        // window.Messprogramm and respective controller
-        var mstLaborKombiStore = Ext.create('Ext.data.Store', {
-            storeId: 'messstellelaborkombi',
-            // Model without proxy. Data added in load-callback
-            // on store 'messstellenkombi' created further down.
-            model: 'Lada.model.MessstelleLabor'
-        });
-
         Ext.create('Lada.store.Datenbasis', {
             storeId: 'datenbasis'
         });
@@ -269,15 +249,13 @@ Ext.application({
         // window.Ortszuordnung, window.GenProbenFromMessprogramm,
         // window.ProbeEdit, window.MessungCreate, panel.Map, panel.QueryPanel,
         // grid.PKommentar, ...
-        // Also used in load-callback on store 'messstellenkombi'.
-        // Load-callback here fills data into stores 'leitstellenwidget'
-        // and 'messstellelabor'.
+        // Load-callback here fills data into store 'leitstellenwidget'.
         // Server service: MessstelleService via model.Messstelle
         Ext.create('Lada.store.Messstellen', {
             storeId: 'messstellen',
             listeners: {
                 load: {
-                    fn: function(store) {
+                    fn: function() {
                         var lst = Ext.data.StoreManager.get(
                                      'leitstellenwidget');
                         lst.removeAll(true);
@@ -290,27 +268,6 @@ Ext.application({
                             }
                         });
                         lst.add(reclst);
-                        for (var j = 0; j < mstLabor.length; j++) {
-                            var item = store.getById(mstLabor[j].messstelle);
-                            var itemLabor = store.getById(mstLabor[j].labor);
-                            if (!itemLabor) {
-                                continue;
-                            }
-                            var displayCombi = item.get('messStelle');
-                            if (item.get('messStelle')
-                                !== itemLabor.get('messStelle')
-                            ) {
-                                displayCombi += '/'
-                                    + itemLabor.get('messStelle');
-                            }
-                            mstLaborStore.add({
-                                id: j,
-                                messStelle: mstLabor[j].messstelle,
-                                netzbetreiberId: item.get('netzbetreiberId'),
-                                laborMst: mstLabor[j].labor,
-                                displayCombi: displayCombi
-                            });
-                        }
                     }
                 }
             }
@@ -464,54 +421,6 @@ Ext.application({
             }
         });
 
-        // Only used to fill data into store 'messstellelaborkombi' via
-        // load-callback.
-        // Server service: MessstellenkombiService via model.MessstellenKombi
-        Ext.create('Lada.store.MessstellenKombi', {
-            storeId: 'messstellenkombi',
-            autoLoad: true,
-            listeners: {
-                beforeload: function(store, operation) {
-                    operation.setParams({
-                        netzbetreiberId: Lada.netzbetreiber
-                    });
-                },
-                load: {
-                    fn: function(store) {
-                        var z = 0;
-                        for (var j = 0; j < store.getCount(); j++) {
-                            var item = Ext.data.StoreManager.get(
-                                'messstellen').getById(
-                                    store.getAt(j).getData().mstId);
-                            var itemLabor = Ext.data.StoreManager.get(
-                                'messstellen').getById(
-                                    store.getAt(j).getData().laborMstId);
-                            if (!itemLabor) {
-                                continue;
-                            }
-                            var displayCombi = item.get('messStelle');
-                            if (displayCombi !== itemLabor.get('messStelle')) {
-                                displayCombi += '/'
-                                    + itemLabor.get('messStelle');
-                            }
-                            var recordIndex = mstLaborKombiStore.findExact(
-                                'displayCombi', displayCombi);
-                            if (recordIndex === -1) {
-                                mstLaborKombiStore.add({
-                                    id: z,
-                                    messStelle: store.getAt(j).getData().mstId,
-                                    netzbetreiberId: item.get('netzbetreiberId'),
-                                    laborMst: store.getAt(
-                                        j).getData().laborMstId,
-                                    displayCombi: displayCombi
-                                });
-                                z++;
-                            }
-                        }
-                    }
-                }
-            }
-        });
         Ext.create('Ext.data.Store', {
             storeId: 'pagingSizes',
             model: Ext.create('Ext.data.Model', {
--- app/view/widget/MessstelleLabor.js
+++ app/view/widget/MessstelleLabor.js
@@ -7,28 +7,171 @@
  */
 
 /**
- * Combobox for Messstelle/Labor
+ * Container for combobox for Messstelle/Labor, setting hidden form fields
+ * and Netzbetreiber automatically.
  */
 Ext.define('Lada.view.widget.MessstelleLabor', {
-    extend: 'Lada.view.widget.base.ComboBox',
+    extend: 'Ext.form.FieldContainer',
     alias: 'widget.messstellelabor',
-    displayField: 'displayCombi',
-    searchValueField: 'messStelle',
-    valueField: 'id',
-    editable: this.editable || false,
-    forceSelection: true,
-    isFormField: false,
+    requires: [
+        'Lada.store.MessstellenKombi',
+        'Lada.view.widget.Netzbetreiber',
+        'Lada.view.widget.base.ComboBox'
+    ],
 
-    // Enable filtering of comboboxes
-    autoSelect: true,
-    queryMode: 'local',
-    triggerAction: 'all',
-    typeAhead: false,
-    minChars: 0,
+    layout: {
+        type: 'hbox',
+        align: 'stretch'
+    },
+
+    defaults: {
+        flex: 1,
+        margin: '0, 5, 5, 5'
+    },
 
     initComponent: function() {
         var i18n = Lada.getApplication().bundle;
-        this.emptyText = i18n.getMsg('emptytext.messstellelabor');
+        var me = this;
+        this.items = [{
+            // Combobox for selection of Messstelle/Labor combination
+            xtype: 'cbox',
+            name: this.name,
+            store: Ext.create('Lada.store.MessstellenKombi', {
+                // autoLoad in case no record is loaded into outer form
+                autoLoad: true
+            }),
+            allowBlank: false,
+            isFormField: false,
+            fieldLabel: i18n.getMsg('labor_mst_id'),
+            labelWidth: 100,
+
+            // Enable filtering of combobox
+            autoSelect: true,
+            queryMode: 'local',
+            triggerAction: 'all',
+            typeAhead: false,
+            minChars: 0,
+            editable: true,
+            forceSelection: true,
+            searchValueField: 'mstId',
+            valueField: 'id',
+            // TODO: Restore displaying names instead of IDs
+            //displayField: 'displayCombi',
+            tpl: Ext.create(
+                'Ext.XTemplate',
+                '<ul class="x-list-plain"><tpl for=".">',
+                '<li role="option" class="x-boundlist-item">',
+                '{mstId}/{laborMstId}',
+                '</li>',
+                '</tpl></ul>'
+            ),
+            displayTpl: Ext.create(
+                'Ext.XTemplate',
+                '<tpl for=".">{mstId}/{laborMstId}</tpl>'),
+            emptyText: i18n.getMsg('emptytext.messstellelabor'),
+
+            // On selection, set other fields in container and assorted
+            // other fields, if available in outer fieldset.
+            listenersJson: {
+                select: {
+                    fn: function(combo, newValue) {
+                        var container = combo.up('fieldcontainer');
+
+                        // Set hidden form fields
+                        ['mstId', 'laborMstId'].forEach(function(id) {
+                            var field = container.down(
+                                'textfield[name=' + id + ']');
+                            field.setValue(newValue.get(id));
+                        });
+
+                        // Set Netzbetreiber
+                        me.setNetzbetreiber(newValue.get('mstId'));
+
+                        // Set related fields, if existent
+                        var fieldset = combo.up('fieldset');
+                        if (fieldset) {
+                            var mplId = fieldset.down(
+                                'messprogrammland[name=mplId]');
+                            if (mplId) {
+                                mplId.setValue();
+                            }
+                            var erzeugerId = fieldset.down(
+                                'datensatzerzeuger[name=erzeugerId]');
+                            if (erzeugerId) {
+                                erzeugerId.setValue();
+                            }
+                        }
+                    }
+                }
+            }
+        }, {
+            xtype: 'netzbetreiber',
+            name: 'netzbetreiber',
+            readOnly: true,
+            isFormField: false,
+            fieldLabel: i18n.getMsg('netzbetreiberId'),
+            labelWidth: 80
+        }, {
+            xtype: 'form',
+            hidden: true,
+            items: [{
+                // Hidden form field for Messstelle
+                xtype: 'textfield',
+                name: 'mstId',
+                allowBlank: false
+            }, {
+                // Hidden form field for Labor
+                xtype: 'textfield',
+                name: 'laborMstId',
+                allowBlank: false
+            }],
+            listeners: {
+                // Intended to be called when a record is loaded into the form
+                validitychange: {
+                    fn: function(hiddenForm, isValid) {
+                        if (isValid) {
+                            var cboxStore = me.down('combobox').getStore();
+                            if (cboxStore.isLoaded()) {
+                                me.setMessstelleLabor();
+                            } else {
+                                cboxStore.load(function(records, op, success) {
+                                    if (!success) {
+                                        Ext.Msg.alert(
+                                            i18n.getMsg(
+                                                'err.msg.generic.title'),
+                                            i18n.getMsg(
+                                                'err.msg.generic.body'));
+                                    }
+                                    me.setMessstelleLabor();
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }];
+
         this.callParent(arguments);
+    },
+
+    setNetzbetreiber: function(mstId) {
+        var mst = Ext.data.StoreManager.get('messstellen').getById(mstId);
+        var nb = mst ? mst.get('netzbetreiberId') : '';
+        this.down('netzbetreiber').setValue(nb);
+    },
+
+    setMessstelleLabor: function() {
+        var mstId = this.down('textfield[name=mstId]').getValue();
+        var laborMstId = this.down('textfield[name=laborMstId]').getValue();
+
+        this.setNetzbetreiber(mstId);
+
+        var cbox = this.down('combobox[name=' + this.name + ']');
+        var selection = cbox.getStore().queryBy(
+            function(rec) {
+                return rec.get('mstId') === mstId
+                    && rec.get('laborMstId') === laborMstId;
+            }).getAt(0);
+        cbox.setValue(selection);
     }
 });
