b38bb2b80fc1b926d52c74d0f98826136d32326e
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-01-05 16:05:54 +0100

    Fix displaying errors in Messung form
    Re-initializing the window also reloaded the record from the server
    into the form, destroying all error messages just put on the fields.

--- app/controller/form/Messung.js
+++ app/controller/form/Messung.js
@@ -123,7 +123,6 @@ Ext.define('Lada.controller.form.Messung', {
                         formPanel.setRecord(record);
                         formPanel.setMessages(json.errors, json.warnings,
                             json.notifications);
-                        formPanel.up('window').initData();
                     } else {
                         Ext.Msg.alert(i18n.getMsg('err.msg.save.title'),
                             i18n.getMsg('err.msg.response.body'));
