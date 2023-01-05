fcd9b57c5f1e3a7625483c8e8a78f2dddcf5a9bb
Alexander Woestmann <awoestmann@intevation.de>
2023-01-05 11:44:09 +0100

    Add missing SiteClass model

---
+++ app/model/SiteClass.js
@@ -0,0 +1,19 @@
+/* Copyright (C) 2013 by Bundesamt fuer Strahlenschutz
+ * Software engineering by Intevation GmbH
+ *
+ * This file is Free Software under the GNU GPL (v>=3)
+ * and comes with ABSOLUTELY NO WARRANTY! Check out
+ * the documentation coming with IMIS-Labordaten-Application for details.
+ */
+
+Ext.define('Lada.model.SiteClass', {
+    extend: 'Lada.model.LadaBase',
+
+    fields: [{
+        name: 'id'
+    }, {
+        name: 'name'
+    }, {
+        name: 'extId'
+    }]
+});
