20c3d794c738bccf1abfa3dab2847a0c2173cf57
Alexander Woestmann <awoestmann@intevation.de>
2023-01-06 12:34:09 +0100

    Fix EnvDescrip widget template

--- app/view/widget/Deskriptor.js
+++ app/view/widget/Deskriptor.js
@@ -26,11 +26,11 @@ Ext.define('Lada.view.widget.Deskriptor', {
     forceSelection: true,
     tpl: Ext.create('Ext.XTemplate',
         '<tpl for="."><div class="x-combo-list-item  x-boundlist-item" >' +
-        '<tpl if="sn &gt; 9">{levVal} - {name}</tpl>',
-        '<tpl if="sn &lt; 10">0{levVal} - {name}</tpl></div></tpl>'),
+        '<tpl if="levVal &gt; 9">{levVal} - {name}</tpl>',
+        '<tpl if="levVal &lt; 10">0{levVal} - {name}</tpl></div></tpl>'),
     displayTpl: Ext.create('Ext.XTemplate',
-        '<tpl for="."><tpl if="sn &gt; 9">{levVal} - {name}</tpl>',
-        '<tpl if="sn &gt; 0 &amp;&amp;sn &lt; 10">0{levVal} - {name}</tpl>',
+        '<tpl for="."><tpl if="levVal &gt; 9">{levVal} - {name}</tpl>',
+        '<tpl if="levVal &gt; 0 &amp;&amp;levVal &lt; 10">0{levVal} - {name}</tpl>',
         '</tpl>'),
 
     initComponent: function() {
