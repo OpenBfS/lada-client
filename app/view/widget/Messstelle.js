e73d1d277e5ebe48cdf0df967c106b8c7591fc46
Alexander Woestmann <awoestmann@intevation.de>
2023-01-06 13:33:31 +0100

    Fix model references in MeasFacil widget

--- app/view/widget/Messstelle.js
+++ app/view/widget/Messstelle.js
@@ -27,11 +27,11 @@ Ext.define('Lada.view.widget.Messstelle', {
     filteredStore: false,
     tpl: Ext.create('Ext.XTemplate',
         '<tpl for="."><div class="x-combo-list-item  x-boundlist-item" >' +
-            '{id} - {messStelle}</div></tpl>'),
+            '{id} - {name}</div></tpl>'),
     displayTpl: Ext.create('Ext.XTemplate',
-        '<tpl for=".">{id} - {messStelle}</tpl>'),
+        '<tpl for=".">{id} - {name}</tpl>'),
     labelTpl: Ext.create('Ext.XTemplate',
-        '<tpl for=".">{id} - {messStelle}</tpl>'),
+        '<tpl for=".">{id} - {name}</tpl>'),
 
     initComponent: function() {
         var i18n = Lada.getApplication().bundle;
