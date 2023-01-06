4fd29f2caf10843b49d44b6f9df5d85afb48810c
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-01-06 17:04:37 +0100

    Disabled state is finally handled by controller

--- app/view/form/Ort.js
+++ app/view/form/Ort.js
@@ -220,9 +220,7 @@ Ext.define('Lada.view.form.Ort', {
                 action: 'copy',
                 qtip: i18n.getMsg('copy.qtip', i18n.getMsg('ort')),
                 icon: 'resources/img/dialog-ok-apply.png',
-                disabled: !this.record.phantom && !this.record.get('readonly') ?
-                    false :
-                    true
+                disabled: true
             },
             '->',
             {
