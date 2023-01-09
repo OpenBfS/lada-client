cff988b00526881907a428124b5d3706720a7026
Alexander Woestmann <awoestmann@intevation.de>
2023-01-09 13:24:14 +0100

    Fix sorting field reference in site store

--- app/store/Orte.js
+++ app/store/Orte.js
@@ -13,7 +13,7 @@ Ext.define('Lada.store.Orte', {
     extend: 'Ext.data.Store',
     model: 'Lada.model.Site',
     sorters: [{
-        property: 'ortId',
+        property: 'extId',
         direction: 'ASC'
     }],
     remoteFilter: true,
