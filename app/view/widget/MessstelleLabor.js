083a8ff398986bcb5b329d08acceef66a49bd048
Alexander Woestmann <awoestmann@intevation.de>
2023-01-10 11:34:42 +0100

    Fix DatasetCreator, Sampler and MpgCateg combobox filtering in mpg and sample form

--- app/view/widget/MessstelleLabor.js
+++ app/view/widget/MessstelleLabor.js
@@ -164,6 +164,12 @@ Ext.define('Lada.view.widget.MessstelleLabor', {
         this.callParent(arguments);
     },
 
+    getNetworkId: function() {
+        var mstId = this.down('textfield[name=measFacilId]').getValue();
+        var mst = Ext.data.StoreManager.get('messstellen').getById(mstId);
+        return mst.get('networkId');
+    },
+
     setNetzbetreiber: function(mstId) {
         var mst = Ext.data.StoreManager.get('messstellen').getById(mstId);
         var nbId = mst.get('networkId');
