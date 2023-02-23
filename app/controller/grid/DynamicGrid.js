486dec828f130dde83953a2654c207033ff043b0
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-02-23 16:11:29 +0100

    Move code from legacy controller to where it actually belongs

--- app/controller/grid/DynamicGrid.js
+++ app/controller/grid/DynamicGrid.js
@@ -13,6 +13,7 @@ Ext.define('Lada.controller.grid.DynamicGrid', {
     extend: 'Ext.app.Controller',
     requires: [
         'Lada.view.window.DeleteMultipleItems',
+        'Lada.view.window.GenProbenFromMessprogramm',
         'Lada.view.window.ProbeCreate',
         'Lada.view.window.SetTags',
         'Lada.view.window.TagManagement'
@@ -41,6 +42,15 @@ Ext.define('Lada.controller.grid.DynamicGrid', {
             },
             'button[action=genericadd]': {
                 click: this.addData
+            },
+            'dynamicgrid toolbar button[action=genProbenFromMessprogramm]': {
+                click: this.genProbenFromMessprogramm
+            },
+            'dynamicgrid toolbar button [action=setActiveNo]': {
+                click: this.setActiveNo
+            },
+            'dynamicgrid toolbar button [action=setActiveYes]': {
+                click: this.setActiveYes
             },
             'button[action=addMap]': {
                 click: this.activateDraw
@@ -413,6 +423,119 @@ Ext.define('Lada.controller.grid.DynamicGrid', {
                         parentGrid: grid }
                     ).show();
             }
+        }
+    },
+
+    /**
+     * This button creates a window to generate Proben
+     * from a selected messprogramm.
+     */
+    genProbenFromMessprogramm: function(button) {
+        var grid = button.up('grid');
+        var selection = grid.getView().getSelectionModel().getSelection();
+        var ids = [];
+        for (var i = 0; i < selection.length; i++) {
+            ids.push(selection[i].data[grid.rowtarget.dataIndex]);
+        }
+        var win = Ext.create('Lada.view.window.GenProbenFromMessprogramm', {
+            ids: ids,
+            parentWindow: grid
+        });
+        win.show();
+    },
+
+    setActiveNo: function(button) {
+        this.doSetActive(false, button);
+    },
+    setActiveYes: function(button) {
+        this.doSetActive(true, button);
+    },
+    doSetActive: function(active, button) {
+        var i18n = Lada.getApplication().bundle;
+        var ids = [];
+        var grid = button.up('grid');
+        var selection = grid.getView().getSelectionModel().getSelection();
+        for (var i = 0; i < selection.length; i++) {
+            ids.push(selection[i].data[grid.rowtarget.dataIndex]);
+        }
+        if (ids.length) {
+            Ext.Ajax.request({
+                url: 'lada-server/rest/messprogramm/aktiv',
+                method: 'PUT',
+                jsonData: {
+                    aktiv: active,
+                    ids: ids
+                },
+                success: function(response) {
+                    var json = Ext.JSON.decode(response.responseText);
+                    var resultMessage = '';
+                    for (var j = 0; j < json.data.length; j++) {
+                        if (json.data[j].success !== 200) {
+                            resultMessage += '<strong>'
+                                + i18n.getMsg('messprogramm') + ': '
+                                + json.data[j].id
+                                + '</strong><br><dd>'
+                                + i18n.getMsg(json.data[j].success)
+                                + '</dd><br>';
+                        }
+                    }
+                    if (resultMessage) {
+                        var errorWin = Ext.create('Ext.window.Window', {
+                            title: i18n.getMsg('setActiveMp.failure.title'),
+                            modal: true,
+                            layout: 'vbox',
+                            width: 340,
+                            height: 165,
+                            autoScroll: true,
+                            items: [{
+                                xtype: 'container',
+                                html: resultMessage,
+                                margin: '10, 5, 5, 5'
+                            }, {
+                                xtype: 'container',
+                                layout: 'hbox',
+                                items: [{
+                                    xtype: 'button',
+                                    text: i18n.getMsg('close'),
+                                    margin: '5, 0, 5, 5',
+                                    handler: function() {
+                                        errorWin.close();
+                                    }
+                                }]
+                            }]
+                        });
+                        errorWin.show();
+                    }
+                    var grids = Ext.ComponentQuery.query('dynamicgrid');
+                    if (grids.length) {
+                        grids[0].reload();
+                    }
+                },
+                failure: function() {
+                    var errorWin = Ext.create('Ext.window.Window', {
+                        title: i18n.getMsg('setActiveMp.failure.title'),
+                        modal: true,
+                        layout: 'vbox',
+                        items: [{
+                            xtype: 'container',
+                            html: i18n.getMsg('export.failednoreason'),
+                            margin: '10, 5, 5, 5'
+                        }, {
+                            xtype: 'container',
+                            layout: 'hbox',
+                            items: [{
+                                xtype: 'button',
+                                text: i18n.getMsg('close'),
+                                margin: '5, 0, 5, 5',
+                                handler: function() {
+                                    errorWin.close();
+                                }
+                            }]
+                        }]
+                    });
+                    errorWin.show();
+                }
+            });
         }
     },
 
