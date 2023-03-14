06607b26cde1e182ab48d4252bc09ebb478a80fc
Tom Gottfried <Tom Gottfried tom@intevation.de>
2023-03-14 18:19:32 +0100

    Make initialization of widget less weird

--- app/view/widget/Statuskombi.js
+++ app/view/widget/Statuskombi.js
@@ -34,11 +34,10 @@ Ext.define('Lada.view.widget.Statuskombi', {
     initComponent: function() {
         this.textFieldCls = 'status-empty';
         this.callParent(arguments);
-    },
 
-    changebutton: function() {
         var i18n = Lada.getApplication().bundle;
-        var btn = Ext.create('Ext.Button', {
+        this.add({
+            xtype: 'button',
             text: i18n.getMsg('button.changestatus'),
             tooltip: i18n.getMsg('button.changestatus.qtip'),
             action: 'newstatus',
@@ -46,7 +45,6 @@ Ext.define('Lada.view.widget.Statuskombi', {
             disabled: true,
             listeners: this.buttonListener
         });
-        return btn;
     },
 
 
@@ -87,12 +85,6 @@ Ext.define('Lada.view.widget.Statuskombi', {
 
             }
         });
-        // instead of overwriting/appending initComponent, add the button at
-        // loading of values
-        var button = this.down('button[action=newstatus]');
-        if (!button) {
-            this.add(this.changebutton());
-        }
     },
 
 
