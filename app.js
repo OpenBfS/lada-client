bb59773e17c29ee73c0dbdc127fbf32f660078fd
Michael Stanko <mstanko@bfs.de>
2023-03-16 12:37:29 +0100

    set Version to 4.7.0-SNAPSHOT

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ Ext.application({
         Lada.logintime = '';
         Lada.mst = [];
         Lada.netzbetreiber = [];
-        Lada.clientVersion = '4.6.0-SNAPSHOT';
+        Lada.clientVersion = '4.7.0-SNAPSHOT';
         Lada.serverVersion = '';
         // paging sizes available for the client
         Lada.availablePagingSizes = [
