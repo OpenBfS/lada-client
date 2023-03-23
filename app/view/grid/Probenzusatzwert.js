13b090adca13ad0c2d8adccf881a44634df82bba
Alexander Woestmann <awoestmann@intevation.de>
2023-03-23 14:59:31 +0100

    Update sampleSpecif grid to use BaseGrid validationResultRenderer

--- app/view/grid/Probenzusatzwert.js
+++ app/view/grid/Probenzusatzwert.js
@@ -80,102 +80,110 @@ Ext.define('Lada.view.grid.Probenzusatzwert', {
                 action: 'delete'
             }]
         }];
-        this.columns = [{
-            header: i18n.getMsg('pzs_id'),
-            dataIndex: 'sampleSpecifId',
-            flex: 1
-        }, {
-            header: i18n.getMsg('pzw'),
-            dataIndex: 'sampleSpecifId',
-            flex: 3,
-            renderer: function(value) {
-                if (!value || value === '') {
-                    return '';
+        this.columns = {
+            items: [{
+                header: i18n.getMsg('pzs_id'),
+                dataIndex: 'sampleSpecifId',
+                flex: 1
+            }, {
+                header: i18n.getMsg('pzw'),
+                dataIndex: 'sampleSpecifId',
+                flex: 3,
+                renderer: function(value, metaData, gridRec) {
+                    this.validationResultRenderer(value, metaData, gridRec);
+                    if (!value || value === '') {
+                        return '';
+                    }
+                    var store = Ext.data.StoreManager.get('probenzusaetze');
+                    var record = store.getById(value);
+                    return record.get('name');
+                },
+                editor: {
+                    xtype: 'combobox',
+                    store: this.pzStore,
+                    displayField: 'name',
+                    name: 'name',
+                    valueField: 'id',
+                    allowBlank: false,
+                    editable: true,
+                    queryMode: 'local',
+                    minChars: 0,
+                    disableKeyFilter: false,
+                    matchFieldWidth: false
                 }
-                var store = Ext.data.StoreManager.get('probenzusaetze');
-                var record = store.getById(value);
-                return record.get('name');
-            },
-            editor: {
-                xtype: 'combobox',
-                store: this.pzStore,
-                displayField: 'name',
-                name: 'name',
-                valueField: 'id',
-                allowBlank: false,
-                editable: true,
-                queryMode: 'local',
-                minChars: 0,
-                disableKeyFilter: false,
-                matchFieldWidth: false
-            }
-        }, {
-            header: i18n.getMsg('pzwKleinerALs'),
-            width: 50,
-            dataIndex: 'smallerThan',
-            editor: {
-                xtype: 'checkbox',
-                uncheckedValue: false,
-                inputValue: '<'
-            }
-        }, {
-            header: i18n.getMsg('messwert'),
-            dataIndex: 'measVal',
-            flex: 1,
-            editor: {
-                xtype: 'expnumberfield'
-            },
-            renderer: function(value) {
-                if (!value || value === '') {
-                    return value;
+            }, {
+                header: i18n.getMsg('pzwKleinerALs'),
+                width: 50,
+                dataIndex: 'smallerThan',
+                editor: {
+                    xtype: 'checkbox',
+                    uncheckedValue: false,
+                    inputValue: '<'
                 }
-                var strValue = Lada.getApplication().toExponentialString(
-                    value, 2)
-                    .replace('.', Ext.util.Format.decimalSeparator);
-                var splitted = strValue.split('e');
-                var exponent = parseInt(splitted[1], 10);
-                return splitted[0] + 'e'
+            }, {
+                header: i18n.getMsg('messwert'),
+                dataIndex: 'measVal',
+                flex: 1,
+                editor: {
+                    xtype: 'expnumberfield'
+                },
+                renderer: function(value, metaData, gridRec) {
+                    this.validationResultRenderer(value, metaData, gridRec);
+                    if (!value || value === '') {
+                        return value;
+                    }
+                    var strValue = Lada.getApplication().toExponentialString(
+                        value, 2)
+                        .replace('.', Ext.util.Format.decimalSeparator);
+                    var splitted = strValue.split('e');
+                    var exponent = parseInt(splitted[1], 10);
+                    return splitted[0] + 'e'
                     + ((exponent < 0) ? '-' : '+')
                     + ((Math.abs(exponent) < 10) ? '0' : '')
                     + Math.abs(exponent).toString();
-            }
-        }, {
-            header: i18n.getMsg('mehId'),
-            dataIndex: 'sampleSpecifId',
-            flex: 1,
-            renderer: function(value) {
-                if (!value || value === '') {
-                    return '';
                 }
-                var zStore = Ext.data.StoreManager.get('probenzusaetze');
-                var mstore = Ext.data.StoreManager.get('messeinheiten');
-                var mehId = zStore.getById(value).get('unitId');
-                var record = mstore.findRecord(
-                    'id', mehId, 0, false, false, true);
-                if (!record) {
-                    return '';
+            }, {
+                header: i18n.getMsg('mehId'),
+                dataIndex: 'sampleSpecifId',
+                flex: 1,
+                renderer: function(value, metaData, gridRec) {
+                    this.validationResultRenderer(value, metaData, gridRec);
+                    if (!value || value === '') {
+                        return '';
+                    }
+                    var zStore = Ext.data.StoreManager.get('probenzusaetze');
+                    var mstore = Ext.data.StoreManager.get('messeinheiten');
+                    var mehId = zStore.getById(value).get('unitId');
+                    var record = mstore.findRecord(
+                        'id', mehId, 0, false, false, true);
+                    if (!record) {
+                        return '';
+                    }
+                    return record.get('unitSymbol');
                 }
-                return record.get('unitSymbol');
+            }, {
+                header: i18n.getMsg('relmessfehler'),
+                dataIndex: 'error',
+                xtype: 'numbercolumn',
+                format: '0000.0',
+                flex: 1,
+                editor: {
+                    xtype: 'formatnumberfield',
+                    allowBlank: true,
+                    maxLength: 8,
+                    minValue: 0,
+                    maxValue: 1000,
+                    decimalPrecision: 1,
+                    allowDecimals: true,
+                    allowExponential: false,
+                    enforceMaxLength: true,
+                    hideTrigger: true
+                }
+            }],
+            defaults: {
+                renderer: this.validationResultRenderer
             }
-        }, {
-            header: i18n.getMsg('relmessfehler'),
-            dataIndex: 'error',
-            xtype: 'numbercolumn',
-            format: '0000.0',
-            flex: 1,
-            editor: {
-                xtype: 'formatnumberfield',
-                allowBlank: true,
-                maxLength: 8,
-                minValue: 0,
-                maxValue: 1000,
-                decimalPrecision: 1,
-                allowDecimals: true,
-                allowExponential: false,
-                enforceMaxLength: true,
-                hideTrigger: true
-            }
-        }];
+        };
         this.listeners = {
             select: {
                 fn: this.activateRemoveButton,
