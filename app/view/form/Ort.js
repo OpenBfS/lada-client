c298ba005f0fd76b86616a142dc441f54b4d4d69
Alexander Woestmann <awoestmann@intevation.de>
2023-06-15 14:22:25 +0200

    Update site form route field to use a textarea with a maxLenght of 1024

--- app/view/form/Ort.js
+++ app/view/form/Ort.js
@@ -105,9 +105,9 @@ Ext.define('Lada.view.form.Ort', {
             name: 'isReiActive',
             fieldLabel: i18n.getMsg('orte.aktiv')
         }, {
-            xtype: 'tfield',
+            xtype: 'tarea',
             labelWidth: 125,
-            maxLength: 10,
+            maxLength: 1024,
             name: 'route',
             fieldLabel: i18n.getMsg('orte.route')
         }, {
