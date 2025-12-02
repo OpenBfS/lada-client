422c6ffc11ce5d4e47bc61b79dbf5cd6c02d33ec
Tom Gottfried <tom@intevation.de>
2025-12-02 19:09:37 +0100

    Remove noise and trailing whitespace from setup stuff
    Many of the calls to SenchaCmd actually did nothing in the given
    context. Also minimizes the amount of changes to the source
    directory to just putting a symlink to ExtJS into it.

--- docker-build-app.sh
+++ docker-build-app.sh
@@ -1,18 +1,8 @@
 #!/bin/bash -e
 
 BUILD_TYPE="$1"
-CUR_DIR=`pwd`
-WORKSPACE=$CUR_DIR
+
 EXTJS_VERSION="6.2.0"
 
-SENCHA_CMD="/opt/Sencha/sencha"
-SDK_PATH="/usr/local/lada"
-
-ln -sf ${WORKSPACE}/ext-${EXTJS_VERSION} ext
-#Set SDK path. Must be set to the path containing the SDK folder, not the SDK folder itself
-$SENCHA_CMD config --prop sencha.sdk.path=$SDK_PATH --save
-#Init workspace and app
-$SENCHA_CMD workspace init
-$SENCHA_CMD app install
-$SENCHA_CMD app clean
-$SENCHA_CMD app build ${BUILD_TYPE}
+ln -sf /opt/ext-${EXTJS_VERSION} ext
+/opt/Sencha/sencha app build --clean ${BUILD_TYPE}
--- install-sencha2opt.sh
+++ install-sencha2opt.sh
@@ -5,26 +5,25 @@ WORKSPACE=$CUR_DIR
 SENCHA_CMD_VERSION="7.2.0.84"
 EXTJS_VERSION="6.2.0"
 
-if [ ! -e SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh ]
+# Install SenchaCmd
+cd /opt
+SENCHA_INSTALLER=SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh
+if [ ! -e $SENCHA_INSTALLER ]
 then
-  curl -sSL https://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/no-jre/SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip \
-     -o SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
-  unzip -q -n SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
-  rm SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh.zip
+  curl -sSLO https://cdn.sencha.com/cmd/$SENCHA_CMD_VERSION/no-jre/$SENCHA_INSTALLER.zip
+  unzip -q -n $SENCHA_INSTALLER.zip
+  rm $SENCHA_INSTALLER.zip
 fi
-./SenchaCmd-$SENCHA_CMD_VERSION-linux-amd64.sh -q -dir "/opt/$SENCHA_CMD_VERSION"
+./$SENCHA_INSTALLER -q -dir "/opt/$SENCHA_CMD_VERSION"
+ln -s /opt/$SENCHA_CMD_VERSION /opt/Sencha
 
+# Install ExtJS
 if [ ! -e ext-$EXTJS_VERSION ]
 then
-  curl -sSL  https://cdn.sencha.com/ext/gpl/ext-$EXTJS_VERSION-gpl.zip \
-     -o ext-$EXTJS_VERSION-gpl.zip
-  unzip -q -n ext-$EXTJS_VERSION-gpl.zip
-  rm ext-$EXTJS_VERSION-gpl.zip
+  ZIP_FILE=ext-$EXTJS_VERSION-gpl.zip
+  curl -sSLO https://cdn.sencha.com/ext/gpl/$ZIP_FILE
+  unzip -q -n $ZIP_FILE
+  rm $ZIP_FILE
   # Examples not needed but use a lot of disk space
   find ext-$EXTJS_VERSION -name examples -type d -print0 | xargs -0 rm -r
 fi
-
-ln -s /opt/$SENCHA_CMD_VERSION /opt/Sencha
-
-export SENCHA_CMD="/opt/$SENCHA_CMD_VERSION/sencha"
-
