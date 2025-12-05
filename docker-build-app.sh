f7340bfeaf89b3f9fb532ebc5f283ed8b9f3e442
Tom Gottfried <tom@intevation.de>
2025-12-05 13:10:15 +0100

    Improve build script

--- docker-build-app.sh
+++ docker-build-app.sh
@@ -4,5 +4,9 @@ BUILD_TYPE="$1"
 
 EXTJS_VERSION="6.2.0"
 
+# SenchaCmd requires directory "ext" with ExtJS framework in it
 ln -sf /opt/ext-${EXTJS_VERSION} ext
-/opt/Sencha/sencha app build --clean ${BUILD_TYPE}
+
+# Build and force sensible exit code on error
+/opt/Sencha/sencha app build --clean ${BUILD_TYPE} \
+    | grep '^\[ERR\]' && exit 1 || true
