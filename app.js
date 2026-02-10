ef50caed91737ee93fc8fe5b08ac68ae20be68f2
Arne Schumacher <aschumacher@bfs.de>
2026-02-10 10:18:56 +0100

    4.8.10

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ Ext.application({
         Lada.logintime = '';
         Lada.mst = [];
         Lada.netzbetreiber = [];
-        Lada.clientVersion = '4.8.9';
+        Lada.clientVersion = '4.8.10';
         Lada.serverVersion = '';
         // paging sizes available for the client
         Lada.availablePagingSizes = [
